61d1d17dacc08654ffbd6b8b489b04d63ef0f8e8
Far9999 <sadikalhanssah@gmail.com>
2023-05-25 13:30:27 +0000

    what is your full name?

---
+++ What_Your_Name.py
@@ -0,0 +1,9 @@
+def print_full_name(first, last):
+    # Write your code here
+    print(f'Hello {first} {last}! You just delved into python.')
+
+
+
+fName = input("Enter your first name: ")
+lName = input("Enter your last name: ")
+print_full_name(fName, lName)
