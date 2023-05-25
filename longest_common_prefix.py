44653ffa8257a5d84a482f3357a08bce9b95ab60
Far9999 <sadikalhanssah@gmail.com>
2023-05-25 14:07:32 +0000

    Longest common prefix in a list of strings

---
+++ longest_common_prefix.py
@@ -0,0 +1,17 @@
+class Solution:
+    def longestCommonPrefix(self, strs: list[str]) -> str:
+        commonPrefix = ""
+        strs = sorted(strs)
+        firstWord = strs[0]
+        lastWord = strs[-1]
+
+        for i in range(min(len(firstWord), len(lastWord))):
+            if (firstWord[i] != lastWord[i]):
+                return commonPrefix
+            commonPrefix+=firstWord[i]
+        return commonPrefix
+    
+    
+    
+obj = Solution
+print(obj.longestCommonPrefix(obj,strs=["Hello", "Hey", "Henok"]))
