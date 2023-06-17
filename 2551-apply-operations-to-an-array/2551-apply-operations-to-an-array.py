83419001392fa4557ea9dd41881a3fe86489229f
Far9999 <108911044+Far9999@users.noreply.github.com>
2023-06-17 15:27:44 +0000

    Time: 70 ms (40.33%), Space: 16.7 MB (5.62%) - LeetHub

---
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def applyOperations(self, nums: List[int]) -> List[int]:
+        for i in range(len(nums)-1):
+            if nums[i]==nums[i+1]:
+                nums[i]*=2
+                nums[i+1]=0
+        nums.sort(key=lambda x:x==0)  
+        return nums
