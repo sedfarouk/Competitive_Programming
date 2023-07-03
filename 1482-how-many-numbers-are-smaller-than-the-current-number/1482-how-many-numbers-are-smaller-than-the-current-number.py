e7f7fcc31adf45c64ca80fbe314fa5a3fa0458d3
Farouk Sedick <sadikalhanssah@gmail.com>
2023-07-03 17:38:37 +0000

    Delete 1482-how-many-numbers-are-smaller-than-the-current-number directory

--- 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
-        cnt = [0]*(max(nums)+1)
-        ans = []
-        for i in nums:
-            cnt[i]+=1
-        for i in nums:
-            ans.append(sum(cnt[:i]))
-        return ans
