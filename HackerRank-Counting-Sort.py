55c7ece7c75572f93d6ae9895276985392dab303
Farouk Sedick <sadikalhanssah@gmail.com>
2023-07-03 13:52:50 +0000

    Create HackerRank-Counting-Sort.py

---
+++ HackerRank-Counting-Sort.py
@@ -0,0 +1,37 @@
+# HackerRank - Counting sort
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def countingSort(arr):
+    # Write your code here
+    cnt = [0]*100
+    for i in range(len(arr)):
+        cnt[arr[i]]+=1
+    return cnt
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
