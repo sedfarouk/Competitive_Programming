f584a99098865940048206eca160b0d1bb6faaa9
Farouk Sedick <sadikalhanssah@gmail.com>
2023-07-03 05:52:21 +0000

    Delete 1848-sum-of-unique-elements directory

--- 1848-sum-of-unique-elements/1848-sum-of-unique-elements.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def sumOfUnique(self, nums: List[int]) -> int:
-        ans =0
-        set_nums = list(set(nums))
-        for i in set_nums:
-            if nums.count(i)==1:
-                ans+=i
-        return ans
