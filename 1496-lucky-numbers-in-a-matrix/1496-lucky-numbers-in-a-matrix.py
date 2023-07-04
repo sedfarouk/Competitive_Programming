d24e03aeaa6795b43576d461e7c88b9c6f720b55
Farouk Sedick <sadikalhanssah@gmail.com>
2023-07-04 06:32:20 +0000

    Time: 145 ms (53.77%), Space: 16.8 MB (29.71%) - LeetHub

--- 1496-lucky-numbers-in-a-matrix/1496-lucky-numbers-in-a-matrix.py
+++ 1496-lucky-numbers-in-a-matrix/1496-lucky-numbers-in-a-matrix.py
@@ -3,10 +3,10 @@ class Solution:
         min_nums, max_nums, ans = [], [], []
         cols = list(map(list, zip(*matrix)))
         for i in range(len(matrix)):
-            min_nums.append(min(set(matrix[i])))
+            min_nums.append(min(matrix[i]))
 
         for j in range(len(matrix[0])):
-            max_nums.append(max(set(cols[j])))
+            max_nums.append(max(cols[j]))
         
         for i in min_nums:
             if i in max_nums:
