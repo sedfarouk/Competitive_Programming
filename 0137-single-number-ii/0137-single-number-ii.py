446dd1d37604fbafc9925afca90c5ddee9348d38
Farouk Sedick <sadikalhanssah@gmail.com>
2023-07-09 11:38:51 +0000

    Time: 67 ms (88.97%), Space: 18.3 MB (38.41%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -1,5 +1,3 @@
 class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
-        for i in nums:
-            if nums.count(i)==1:
-                return i
+    def singleNumber(self,nums:List[int])->int:
+        return (sum(set(nums))*3 -sum(nums))//2        
