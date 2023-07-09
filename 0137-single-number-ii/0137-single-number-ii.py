e5ea56df48e797dc7afff814f3ef446d7099b79f
Farouk Sedick <sadikalhanssah@gmail.com>
2023-07-09 11:20:57 +0000

    Time: 1237 ms (5.02%), Space: 18.4 MB (21.6%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -1,3 +1,6 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        return int(''.join([str(i) for i in nums if nums.count(i)==1]))
+        unique = list(set(nums))
+        for i in unique:
+            if nums.count(i)==1:
+                return i
