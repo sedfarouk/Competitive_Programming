b69c96c0db2441a6ee287df44f5c506763a6c3fe
Farouk Sedick <sadikalhanssah@gmail.com>
2023-07-12 05:28:16 +0000

    Time: 50 ms (83.45%), Space: 16.2 MB (91.72%) - LeetHub

---
+++ 0179-largest-number/0179-largest-number.py
@@ -0,0 +1,15 @@
+class LargerNum(str):
+    def __lt__(a, b):
+        return a+b > b+a
+
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        nums = [str(num) for num in nums]
+        nums.sort(key=LargerNum)
+        ans = ''.join(nums)
+
+        if ans[0]=="0":
+            return "0"
+        return ans
+
+        
