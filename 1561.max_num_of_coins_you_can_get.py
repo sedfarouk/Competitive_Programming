b6e9be8c30b67ae47f8c28f07cbb29195930fdd5
Farouk Sedick <sadikalhanssah@gmail.com>
2023-07-12 05:05:13 +0000

    Add files via upload
    Initial commit

---
+++ 1561.max_num_of_coins_you_can_get.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxCoins(self, piles: list[int]) -> int:
+        piles.sort()
+        l = int(len(piles)/3)
+        ans = 0
+        cnt = 0
+        for i in range(len(piles)-2,0,-2):
+            ans+=piles[i]
+            cnt+=1
+            if cnt==l:
+                return ans
+            
