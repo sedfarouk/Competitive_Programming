ff04a85803e19189df6a231ee4184c3dbf774230
Farouk Sedick <sadikalhanssah@gmail.com>
2023-07-20 02:19:32 +0000

    Create 189-Rotate_the_array.py

---
+++ 189-Rotate_the_array.py
@@ -0,0 +1,11 @@
+# 189. Leetcode - Rotate the array
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k = k % len(nums)
+        nums.reverse()
+        nums[:k] = reversed(nums[:k])
+        nums[k:] = reversed(nums[k:])
