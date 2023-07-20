b47523bd0722c41e676a42d9a6a2723a334997f2
Farouk Sedick <sadikalhanssah@gmail.com>
2023-07-20 02:24:46 +0000

    Delete 0189-rotate-array directory
    delete

--- 0189-rotate-array/0189-rotate-array.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def rotate(self, nums: List[int], k: int) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        for i in range(k):
-            nums.insert(0, nums[-1])
-            nums.pop()
