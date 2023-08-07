5dbff5ded219baba7112d93967c0b62af189cf17
Farouk Sedick <sadikalhanssah@gmail.com>
2023-08-07 12:57:00 +0000

    Time: 43 ms (73.08%), Space: 16.3 MB (75.89%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.py
@@ -0,0 +1,17 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        return x**n
+        # Using faster exponentiation formula with recursion
+
+        # def function(base=x, exp=abs(n)):
+        #     if exp==0:
+        #         return 1
+        #     elif exp % 2 == 0:
+        #         return function(base*base, (exp)//2)
+        #     return base * function(base*base, (exp-1)//2)
+        
+        # f = function()
+        # if n>=0: 
+        #     return float(f)
+        # return 1/f
+
