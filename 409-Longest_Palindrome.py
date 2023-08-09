940090ce283240df8055b5de125f15f7a18af0f5
Farouk Sedick <sadikalhanssah@gmail.com>
2023-08-09 16:12:00 +0000

    Update 409-Longest_Palindrome.py

--- 409-Longest_Palindrome.py
+++ 409-Longest_Palindrome.py
@@ -4,21 +4,12 @@ class Solution:
     def longestPalindrome(self, s: str) -> int:
         hashmap = Counter(s)
         evens, odds = [], []
+        ans, odd_present = 0, 0
 
         for i in hashmap.values():
             if i%2==0:
-                evens.append(i)
+                ans+=i
             else:
-                odds.append(i)
-
-        sum_evens = sum(evens)
-
-        odds_sum = 0
-        for i in odds:
-            if i>1:
-                odds_sum+=i-1
-
-        if odds!=[]:
-            return sum_evens + odds_sum + 1
-        return sum_evens
-            
+                ans+=i-1
+                odd_present = 1
+        return ans + odd_present
