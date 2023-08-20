b77ab10c028e013d65186259bd5d85ad17163638
Farouk Sedick <sadikalhanssah@gmail.com>
2023-08-20 20:25:34 +0000

    Time: 45 ms (80.16%), Space: 16.3 MB (35.33%) - LeetHub

--- 0768-partition-labels/0768-partition-labels.py
+++ 0768-partition-labels/0768-partition-labels.py
@@ -1,31 +1,42 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        ans = []
-        hashmap = {}
-        s_reversed = list(reversed(s))
-        s_set = list(set([i for i in s]))
+        last = {c:i for i,c in enumerate(s)}
+        start = end = 0
+        output = []
+        for i,c in enumerate(s):
+            end = max(end,last[c])
+            if i == end:
+                output.append(end-start+1)
+                start = i + 1
+        return output
 
-        for i in s_set:
-            hashmap[i] = [s.index(i)]
 
-        for i in s_set:
-            hashmap[i].append(len(s) - s_reversed.index(i)-1)
+        # ans = []
+        # hashmap = {}
+        # s_reversed = list(reversed(s))
+        # s_set = list(set([i for i in s]))
 
-        vals = sorted(hashmap.values(), key=lambda x:x[0])
+        # for i in s_set:
+        #     hashmap[i] = [s.index(i)]
+
+        # for i in s_set:
+        #     hashmap[i].append(len(s) - s_reversed.index(i)-1)
+
+        # vals = sorted(hashmap.values(), key=lambda x:x[0])
         
-        max_num = vals[0][1]
-        sub = vals[0][0]
-        for i in range(1, len(vals)):
-            if vals[i][0] <= max_num:
-                max_num = max(max_num, vals[i][1])
-            else:
-                ans.append(max_num-sub+1)
-                sub = vals[i][0]
-                max_num = vals[i][1]
+        # max_num = vals[0][1]
+        # sub = vals[0][0]
+        # for i in range(1, len(vals)):
+        #     if vals[i][0] <= max_num:
+        #         max_num = max(max_num, vals[i][1])
+        #     else:
+        #         ans.append(max_num-sub+1)
+        #         sub = vals[i][0]
+        #         max_num = vals[i][1]
         
-        ans.append(max_num-sub+1)
+        # ans.append(max_num-sub+1)
 
-        return ans
+        # return ans
             
         
         
