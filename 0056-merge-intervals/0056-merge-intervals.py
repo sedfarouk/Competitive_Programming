0726061b6b2a952030850d6f5be7d666b432461c
Farouk Sedick <sadikalhanssah@gmail.com>
2023-08-20 20:57:50 +0000

    Time: 118 ms (99.78%), Space: 21 MB (81.59%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -5,15 +5,21 @@ class Solution:
         max_val = intervals[0][1]
         min_val = intervals[0][0]
 
-        for i in range(1, len(intervals)):
-            if intervals[i][0] <= max_val:
-                max_val = max(max_val, intervals[i][1])
-            else:
-                ans.append([min_val, max_val])
-                min_val = intervals[i][0]
-                max_val = intervals[i][1]
+        # for i in range(1, len(intervals)):
+        #     if intervals[i][0] <= max_val:
+        #         max_val = max(max_val, intervals[i][1])
+        #     else:
+        #         ans.append([min_val, max_val])
+        #         min_val = intervals[i][0]
+        #         max_val = intervals[i][1]
 
-        ans.append([min_val, max_val])
+        # ans.append([min_val, max_val])
+
+        for i in intervals:
+            if not ans or ans[-1][-1] < i[0]:
+                ans.append(i)
+            else:
+                ans[-1][1] = max(ans[-1][1], i[1])
 
         return ans
 
