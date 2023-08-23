8244d76069166c7455294b193f1e686c02c2bee0
Farouk Sedick <sadikalhanssah@gmail.com>
2023-08-23 12:59:55 +0000

    Time: 526 ms (45.33%), Space: 16.5 MB (70.11%) - LeetHub

---
+++ 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.py
@@ -0,0 +1,21 @@
+class Solution:
+    def countCompleteSubarrays(self, nums: List[int]) -> int:
+        distinct = len(set(nums))
+        ans = 0
+        win = []
+
+        for i in range(len(nums)):
+            win = nums[:i+1]
+
+            if len(set(win))==distinct:
+                hashmap = Counter(win)
+                for j in win:
+                    if hashmap[j] > 1:
+                        ans+=1
+                        hashmap[j]-=1
+                    elif hashmap[j] == 1:
+                        ans+=1
+                        break
+                        
+        return ans
+        
