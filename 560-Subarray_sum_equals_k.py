3c773789d8216e426209ca2a59f8a313e4b029dd
Farouk Sedick <sadikalhanssah@gmail.com>
2023-08-30 10:54:41 +0000

    Create 560-Subarray_sum_equals_k.py

---
+++ 560-Subarray_sum_equals_k.py
@@ -0,0 +1,15 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        ans = 0
+        cnt = 0
+
+        hashmap = defaultdict(int)
+        hashmap[0] = 1
+
+        for i in range(len(nums)):
+            cnt+=nums[i]
+            if (cnt-k) in hashmap.keys():
+                ans+=hashmap[cnt-k]
+            hashmap[cnt] += 1
+
+        return ans
