22630719c38cb6333c5f69e5a732fddc616e631c
Farouk Sedick <sadikalhanssah@gmail.com>
2023-08-31 13:04:55 +0000

    Time: 896 ms (90.86%), Space: 32.7 MB (64.01%) - LeetHub

--- 2145-grid-game/2145-grid-game.py
+++ 2145-grid-game/2145-grid-game.py
@@ -1,19 +1,12 @@
 class Solution:
     def gridGame(self, grid: List[List[int]]) -> int:
-        top = [0]*(len(grid[0])+2)
-        down = [0]*(len(grid[0])+2)
+        top = sum(grid[0])
+        bottom = 0
+        ans = float("inf")
 
-        # Prefix sum for bottom row
-        for i in range(1, len(down)-1):
-            down[i] = grid[1][i-1] + down[i-1]
-        
-        # Suffix sum for top row
-        for i in range(len(top)-2, 0, -1):
-            top[i] = grid[0][i-1] + top[i+1]
- 
-        res = []
-        # Find best answers
-        for i in range(1, len(top)-1):
-            res.append(max(top[i+1], down[i-1]))
-        # return minimum since second robot gets that
-        return min(res)
+        for i in range(len(grid[0])):
+            top-=grid[0][i]
+            ans = min(ans, max(top,bottom))
+            bottom+=grid[1][i]
+
+        return ans
