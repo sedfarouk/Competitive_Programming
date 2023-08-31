672b2a2d4cad37809d5da7d54d304d05e3b9a710
Farouk Sedick <sadikalhanssah@gmail.com>
2023-08-31 13:09:23 +0000

    Create 2017-Grid_game.py

---
+++ 2017-Grid_game.py
@@ -0,0 +1,12 @@
+class Solution:
+    def gridGame(self, grid: List[List[int]]) -> int:
+        top = sum(grid[0])
+        bottom = 0
+        ans = float("inf")
+        
+        for i in range(len(grid[0])):
+            top-=grid[0][i]
+            ans = min(ans, max(top,bottom))
+            bottom+=grid[1][i]
+
+        return ans
