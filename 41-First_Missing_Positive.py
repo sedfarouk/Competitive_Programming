332cf519727be0639f8858d99e81c773fa10bdac
Farouk Sedick <sadikalhanssah@gmail.com>
2023-09-03 22:37:01 +0000

    Create 41-First_Missing_Positive.py

---
+++ 41-First_Missing_Positive.py
@@ -0,0 +1,12 @@
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        unique = set(nums)
+        maxx = max(nums)
+
+        if maxx < 0:
+            return 1
+        
+        for i in range(1, maxx+1):
+            if i not in unique:
+                return i
+        return maxx+1
