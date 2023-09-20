3654d4a1846de3d82627ff43eeae912706be4312
Farouk Sedick <sadikalhanssah@gmail.com>
2023-09-20 11:26:47 +0000

    Time: 34 ms (87.02%), Space: 16.2 MB (86.72%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -4,15 +4,13 @@ class Solution:
     
     def fib(self, n: int) -> int:
         if n in self.hashmap:
-            return self.hashmap[self.fib(n)]
+            return self.hashmap[n]
         if n <= 1:
             return n
-        one = self.fib(n-1)
-        two = self.fib(n-2)
+        self.hashmap[n] = self.fib(n-1) + self.fib(n-2)
         
-        return one + two
-        self.hashmap[n-1] = one
-        self.hashmap[n-2] = two
+        return self.hashmap[n]
+        
         
         
         
