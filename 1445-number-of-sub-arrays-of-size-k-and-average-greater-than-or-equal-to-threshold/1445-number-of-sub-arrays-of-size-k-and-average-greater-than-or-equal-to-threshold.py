d4119785b643620ae670e3882b931cf7ec2d6d11
Farouk Sedick <sadikalhanssah@gmail.com>
2023-09-25 22:33:54 +0000

    Time: 503 ms (82.47%), Space: 29.7 MB (45.66%) - LeetHub

--- 1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.py
+++ 1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.py
@@ -3,18 +3,18 @@ class Solution:
         left = 0
         ans, window = 0, sum(arr[left:k])
 
+        if window/k >= threshold:
+            ans += 1
+
         for right in range(k,len(arr)):
+            window += arr[right]
+            window -= arr[left]
+            left += 1
             avg = window / k
             
             if avg >= threshold:
                 ans += 1
-            
-            window += arr[right]
-            window -= arr[left]
-            left += 1
 
-        if window/k >= threshold:
-            ans += 1
         return ans
 
             
