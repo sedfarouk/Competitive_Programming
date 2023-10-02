55b8f234b06287eccb5d45c6569dea2fddd1ca20
Farouk Sedick <sadikalhanssah@gmail.com>
2023-10-02 20:19:11 +0000

    Time: 200 ms (54.9%), Space: 17.4 MB (65.59%) - LeetHub

--- 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/2149-remove-colored-pieces-if-both-neighbors-are-the-same-color.py
+++ 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/2149-remove-colored-pieces-if-both-neighbors-are-the-same-color.py
@@ -7,11 +7,10 @@ class Solution:
             return False
 
         for i in range(1, n-1):
-            if colors[i]=='A' and colors[i-1]=='A' and colors[i+1]=='A':
-                Alice_picks += 1
-            if colors[i]=='B' and colors[i-1]=='B' and colors[i+1]=='B':
-                Bob_picks += 1
+            if colors[i]==colors[i-1] and colors[i]==colors[i+1]:
+                if colors[i]=='A':
+                    Alice_picks += 1
+                else:
+                    Bob_picks += 1
 
-        if Alice_picks > Bob_picks:
-            return True
-        return False
+        return Alice_picks > Bob_picks
