e8d1a0631adc85a56cee0862b2cd0ac309840784
Farouk Sedick <sadikalhanssah@gmail.com>
2023-10-03 02:04:19 +0000

    Update 1635-number-of-good-pairs.py

--- 1635-number-of-good-pairs/1635-number-of-good-pairs.py
+++ 1635-number-of-good-pairs/1635-number-of-good-pairs.py
@@ -1,15 +1,4 @@
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
-        ans=0
-        nums.sort()
-
-        temp = 1
-        for i in range(1, len(nums)):
-            if nums[i]==nums[i-1]:
-                temp += 1
-            else:
-                ans += sum(range(temp))
-                temp = 1
-        ans += sum(range(temp))
-        
-        return ans
+        return sum(sum(range(i)) for i in Counter(nums).values())
+ 
