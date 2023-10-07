6fe98fe0b96ed3dce47658c40c8eda1371d87db5
Farouk Sedick <sadikalhanssah@gmail.com>
2023-10-07 19:18:50 +0000

    Time: 40 ms (46.36%), Space: 16.3 MB (48.76%) - LeetHub

--- 2481-strictly-palindromic-number/2481-strictly-palindromic-number.py
+++ 2481-strictly-palindromic-number/2481-strictly-palindromic-number.py
@@ -1,9 +1,4 @@
 class Solution:
     def isStrictlyPalindromic(self, n: int) -> bool:
-        for i in range(2, n-1):
-            num = str(bin(i))
-
-            if num != num[::-1]:
-                return False
-        return True
+        return False
         
