d76a34ac30bfe63c34513c6a17fe91b03eddeb97
Farouk Sedick <sadikalhanssah@gmail.com>
2023-12-03 09:38:04 +0000

    Time: 65 ms (51.79%), Space: 16.4 MB (30.54%) - LeetHub

--- 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.py
+++ 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.py
@@ -6,12 +6,7 @@ class Solution:
             x = abs(points[i][0]-points[i-1][0])
             y = abs(points[i][1]-points[i-1][1])
 
-            if x < y:
-                ans += x + abs(points[i][1]-points[i-1][1])-x
-            elif x > y:
-                ans += y + abs(points[i][0]-points[i-1][0])-y
-            else:
-                ans += x
+            ans += max(x, y)
         return ans
 
         
