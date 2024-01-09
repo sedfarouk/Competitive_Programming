35db31ff95c904ff365d64b4531ffdb8fc06c29c
Farouk Sedick <sadikalhanssah@gmail.com>
2024-01-09 18:38:30 +0000

    Time: 45 ms (95.20%), Space: 18.8 MB (13.70%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.py
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.py
@@ -10,30 +10,44 @@ class Node:
 
 class Solution:
     def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
-        ans = []
+        # if not root:
+        #     return root
 
-        if not root:
-            return root
+        # queue = deque([root])
+        # while queue:
+        #     level = len(queue)
+
+        #     for i in range(level):
+        #         q = queue.popleft()
+
+        #         if i==level-1:
+        #             q.next = None
+        #         else:
+        #             q.next = queue[0]
+
+        #         if q.left:
+        #             queue.append(q.left)
+
+        #         if q.right:
+        #             queue.append(q.right)
+        # return root
+        
+        if not root: return root
 
         queue = deque([root])
         while queue:
-            level = len(queue)
+            curr = queue.popleft()
 
-            for i in range(level):
-                q = queue.popleft()
+            if curr.left and curr.right:
+                curr.left.next = curr.right
 
-                if i==level-1:
-                    q.next = None
-                else:
-                    q.next = queue[0]
-
-                if q.left:
-                    queue.append(q.left)
-
-                if q.right:
-                    queue.append(q.right)
+                if curr.next:
+                    curr.right.next = curr.next.left
+                queue.append(curr.left)
+                queue.append(curr.right)
 
         return root
+
 
             
 
