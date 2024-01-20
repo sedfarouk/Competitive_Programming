d457f4bac4c542e18782b126b5a687e7d8985758
Farouk Sedick <sadikalhanssah@gmail.com>
2024-01-20 08:01:26 +0000

    Time: 86 ms (88.79%), Space: 21.2 MB (72.18%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,6 +1,13 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         hashmap = Counter(nums)
-        sorted_keys = sorted(list(hashmap.items()), key = lambda x:x[1], reverse=True)
-        return [sorted_keys[i][0] for i in range(k)]
+        heap = [(-val, key) for key, val in hashmap.items()]
+        heapify(heap)
+        ans = []
+
+        for _ in range(k):
+            freq, val = heappop(heap)
+            ans.append(val)
+            
+        return ans
 
