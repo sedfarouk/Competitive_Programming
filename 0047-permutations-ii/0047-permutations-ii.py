f7d3aafff0205a82d8d745fe6a94f13ae21a22d4
Farouk Sedick <sadikalhanssah@gmail.com>
2024-01-29 03:22:55 +0000

    Time: 55 ms (68.19%), Space: 16.9 MB (66.93%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -0,0 +1,31 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        def is_valid_state(state):
+            return len(state)==len(nums)
+        
+        def get_candidates(count):
+            possible = []
+
+            for x in count:
+                if count[x] > 0:
+                    possible.append(x)
+
+            return possible
+        
+        def search(state, solutions, count):
+            if is_valid_state(state):
+                solutions.append(state.copy())
+                return
+            
+            for candidate in get_candidates(count):
+                state.append(candidate)
+                count[candidate] -= 1
+                search(state, solutions, count)
+                state.pop()
+                count[candidate] += 1
+
+        count
+        solutions = []
+        state = []
+        search(state, solutions, Counter(nums))
+        return solutions
