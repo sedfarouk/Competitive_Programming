459da215ab58e68ba34f3e541696a9669ccd0647
Farouk Sedick <sadikalhanssah@gmail.com>
2024-01-30 07:49:03 +0000

    Time: 481 ms (64.27%), Space: 16.5 MB (63.86%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.py
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.py
@@ -3,19 +3,19 @@ class Solution:
         ans = sum(cookies)
         childrenDistribution = [0] * k
 
-        def distribute(childrenDistribution, idx):
+        def distribute(childrenDistribution, idx, maxx):
             nonlocal ans
             if idx >= len(cookies):
-                ans = min(ans, max(childrenDistribution))
+                ans = min(ans, maxx)
                 return
 
             for i in range(k):
                 childrenDistribution[i] += cookies[idx]
 
                 if childrenDistribution[i] < ans:
-                    distribute(childrenDistribution, idx+1)
+                    distribute(childrenDistribution, idx+1, max(maxx, childrenDistribution[i]))
                 childrenDistribution[i] -= cookies[idx]
 
         cookies.sort(reverse=True)
-        distribute(childrenDistribution, 0)
+        distribute(childrenDistribution, 0, float("-inf"))
         return ans
