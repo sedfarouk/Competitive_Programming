9e286b86f381a81a6d5ffc722aee9c9d445c243d
Farouk Sedick <sadikalhanssah@gmail.com>
2024-02-09 04:11:58 +0000

    Update CodeForces-F-Virus.py

--- CodeForces-F-Virus.py
+++ CodeForces-F-Virus.py
@@ -7,13 +7,11 @@ def solve():
     for i in range(1, m):
         dist.append(inf[i]-inf[i-1]-1)
     dist.append(n-inf[-1]+inf[0]-1)
-   
     dist.sort(reverse=True)
     saved = 0
     
     for i in range(len(dist)):
         x = max(dist[i]-(i*4), 0)
-        
         if x <= 1:
             return n-saved-x
         saved += x-1
