596bdb286c9d9ccdf7c2fabd9e3de2f4d1432da5
Farouk Sedick <sadikalhanssah@gmail.com>
2024-02-09 04:07:03 +0000

    Create CodeForces-F-Virus.py

---
+++ CodeForces-F-Virus.py
@@ -0,0 +1,24 @@
+def solve():
+    n, m = map(int, input().split())
+    inf = list(map(int, input().split()))
+    inf.sort()
+    
+    dist = []
+    for i in range(1, m):
+        dist.append(inf[i]-inf[i-1]-1)
+    dist.append(n-inf[-1]+inf[0]-1)
+   
+    dist.sort(reverse=True)
+    saved = 0
+    
+    for i in range(len(dist)):
+        x = max(dist[i]-(i*4), 0)
+        
+        if x <= 1:
+            return n-saved-x
+        saved += x-1
+    
+    return n-saved
+    
+for _ in range(int(input())):
+    print(solve())
