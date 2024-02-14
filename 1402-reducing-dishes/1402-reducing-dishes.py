ebdf2eaf389c8394019c79f6b42772fd6abf6f16
Farouk Sedick <sadikalhanssah@gmail.com>
2024-02-14 16:22:46 +0000

    Update 1402-reducing-dishes.py

--- 1402-reducing-dishes/1402-reducing-dishes.py
+++ 1402-reducing-dishes/1402-reducing-dishes.py
@@ -1,12 +1,15 @@
 class Solution:
     def maxSatisfaction(self, satisfaction: List[int]) -> int:
         satisfaction.sort(reverse=True)
+        negs, pos = min(0, satisfaction[0]), max(0, satisfaction[0])
         maxx = max(0, satisfaction[0])
                 
         for i in range(1, len(satisfaction)):
-            summ = satisfaction[i] + sum(satisfaction[:i])
+            summ = satisfaction[i] + pos + negs
             if maxx + summ < maxx:
                 return maxx
+            pos += max(0, satisfaction[i])
+            negs += min(0, satisfaction[i])
             maxx += summ 
         return maxx
                 
