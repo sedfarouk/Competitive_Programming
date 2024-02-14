2be879421f156d7d185610690157fde997474109
Farouk Sedick <sadikalhanssah@gmail.com>
2024-02-14 14:21:49 +0000

    Time: 166 ms (81.78%), Space: 18.1 MB (68.22%) - LeetHub

--- 1053-previous-permutation-with-one-swap/1053-previous-permutation-with-one-swap.py
+++ 1053-previous-permutation-with-one-swap/1053-previous-permutation-with-one-swap.py
@@ -1,23 +1,21 @@
 class Solution:
     def prevPermOpt1(self, arr: List[int]) -> List[int]:
-        monostack = deque()
         n = len(arr)
-        prevGreater = [-1]*n
 
-        for i in range(len(arr)-1, -1, -1):
-            while monostack and arr[monostack[-1]] < arr[i]:
-                prevGreater[monostack.pop()] = i
-            monostack.append(i)
-            
-        ans = [-1, -1]
-        for i in range(n-1, -1, -1):
-            if prevGreater[i] != -1:
-                if prevGreater[i]>ans[0] or (prevGreater[i]==ans[0] and arr[ans[1]]==arr[i]): 
-                    ans = [prevGreater[i], i]
-                
-        if ans!=[-1, -1]:
-            arr[ans[0]], arr[ans[1]] = arr[ans[1]], arr[ans[0]]
-            return arr
+        idx = -1
+        for i in range(1, n):
+            if arr[i] < arr[i-1]:
+                idx = i-1
+
+        if idx==-1: return arr
+        
+        pos = idx+1
+        for i in range(idx+1, n):
+            if arr[idx] > arr[i] > arr[pos]:
+                pos = i
+
+        arr[pos], arr[idx] = arr[idx], arr[pos]
         return arr
+        
                     
             
