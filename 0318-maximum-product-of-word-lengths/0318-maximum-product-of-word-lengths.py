5db734e74fe6c48024e9476b0156427412b3f522
Farouk Sedick <sadikalhanssah@gmail.com>
2024-02-15 22:53:58 +0000

    Update 0318-maximum-product-of-word-lengths.py

--- 0318-maximum-product-of-word-lengths/0318-maximum-product-of-word-lengths.py
+++ 0318-maximum-product-of-word-lengths/0318-maximum-product-of-word-lengths.py
@@ -7,10 +7,10 @@ class Solution:
             for ch in word:
                 sett[idx] |= 1 << (ord(ch)-97)
                 
-        maxx = -1
+        maxx = 0
         for i in range(len(words)):
             for j in range(i+1, len(words)):
                 if (sett[i] & sett[j])==0:
                     maxx = max(maxx, len(words[i])*len(words[j]))
-        return 0 if maxx==-1 else maxx
+        return maxx
                 
