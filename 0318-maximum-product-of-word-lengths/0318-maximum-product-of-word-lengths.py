5b23e4d04a9caaf682627a8a9e049b9dc66aed85
Farouk Sedick <sadikalhanssah@gmail.com>
2024-02-15 22:51:27 +0000

    Time: 332 ms (84.98%), Space: 19.7 MB (95.73%) - LeetHub

---
+++ 0318-maximum-product-of-word-lengths/0318-maximum-product-of-word-lengths.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxProduct(self, words: List[str]) -> int:
+        n = len(words)
+        sett = [0]*n
+        
+        for idx, word in enumerate(words):
+            for ch in word:
+                sett[idx] |= 1 << (ord(ch)-97)
+                
+        maxx = -1
+        for i in range(len(words)):
+            for j in range(i+1, len(words)):
+                if (sett[i] & sett[j])==0:
+                    maxx = max(maxx, len(words[i])*len(words[j]))
+        return 0 if maxx==-1 else maxx
+                
