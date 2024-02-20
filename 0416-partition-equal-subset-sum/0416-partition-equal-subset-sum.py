2da759f07966b1783b468f37a034fc578617413d
Farouk Sedick <sadikalhanssah@gmail.com>
2024-02-20 16:28:31 +0000

    Time: 2076 ms (13.98%), Space: 588.9 MB (5.01%) - LeetHub

---
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -0,0 +1,23 @@
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        n = len(nums)
+        memo = {}
+        summ = sum(nums)
+        
+        if summ%2==1: return False
+        
+        def dp(i, first, second):
+            state = (i, first, second)
+            
+            if state in memo: return memo[state]
+            
+            if first==second and first+second==summ:
+                return True
+            if first+second==summ:
+                return False
+            memo[state] = dp(i+1, first+nums[i], second) or dp(i+1, first, second+nums[i])
+            return memo[state]
+        
+        return dp(0, 0, 0)
+            
+            
