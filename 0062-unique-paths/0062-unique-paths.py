b52587f352398b15d9b63523e645093abe02620c
Farouk Sedick <sadikalhanssah@gmail.com>
2024-02-21 11:35:03 +0000

    Update 0062-unique-paths.py

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -4,12 +4,8 @@ class Solution:
 
         def recurse(row, col):
             if (row, col) in memo: return memo[(row, col)]
-            
-            if row==0 and col==0:
-                return 1
-            
-            if row < 0 or col < 0:
-                return 0
+            if row==0 and col==0: return 1
+            if row < 0 or col < 0: return 0
 
             memo[(row, col)] = recurse(row, col-1) + recurse(row-1, col)
             return memo[(row, col)]
