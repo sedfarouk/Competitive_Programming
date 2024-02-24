631088e18b2e6ba4deec4b0f1730861f0c7185f0
Farouk Sedick <sadikalhanssah@gmail.com>
2024-02-24 23:15:09 +0000

    Update 1669H-Maximal_AND.py

--- 1669H-Maximal_AND.py
+++ 1669H-Maximal_AND.py
@@ -1,10 +1,6 @@
 def solve():
     n, k = map(int, input().split())
     nums = list(map(int, input().split()))
-    
-    maxx = 0
-    for num in nums:
-        maxx = max(maxx, num.bit_length())
 
     ans = ""  
     for i in range(30, -1, -1):
