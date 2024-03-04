ac944019950d0276bab745491908dfd74f413f21
Farouk Sedick <sadikalhanssah@gmail.com>
2024-03-04 23:36:29 +0000

    Time: 48 ms (83.26%), Space: 16.8 MB (63.60%) - LeetHub

---
+++ 0948-bag-of-tokens/0948-bag-of-tokens.py
@@ -0,0 +1,14 @@
+class Solution:
+    def bagOfTokensScore(self, tokens: List[int], power: int) -> int:
+        res = cur = 0
+        d = collections.deque(sorted(tokens))
+        
+        while d and (d[0] <= power or cur):
+            if d[0] <= power:
+                power -= d.popleft()
+                cur += 1
+            else:
+                power += d.pop()
+                cur -= 1
+            res = max(res, cur)
+        return res
