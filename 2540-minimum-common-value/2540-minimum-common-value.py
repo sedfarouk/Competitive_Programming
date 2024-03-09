58d0e7e58a2218321f1dd25ea67b0b185a98bb46
Farouk Sedick <sadikalhanssah@gmail.com>
2024-03-09 23:43:58 +0000

    Time: 353 ms (49.49%), Space: 42.7 MB (14.09%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.py
+++ 2540-minimum-common-value/2540-minimum-common-value.py
@@ -1,15 +1,21 @@
 class Solution:
     def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
-        nums1.sort()
-        nums2.sort()
+        cnt1, cnt2 = set(nums1), set(nums2)
+        
+        intersection = cnt1.intersection(cnt2)
+        if not intersection:
+            return -1
+        return min(intersection)
+#         nums1.sort()
+#         nums2.sort()
 
-        nums1_ptr, nums2_ptr = 0, 0
+#         nums1_ptr, nums2_ptr = 0, 0
 
-        while nums1_ptr < len(nums1) and nums2_ptr < len(nums2):
-            if nums1[nums1_ptr]==nums2[nums2_ptr]:
-                return nums1[nums1_ptr]
-            if nums1[nums1_ptr] > nums2[nums2_ptr]:
-                nums2_ptr+=1
-            else:
-                nums1_ptr+=1
-        return -1
+#         while nums1_ptr < len(nums1) and nums2_ptr < len(nums2):
+#             if nums1[nums1_ptr]==nums2[nums2_ptr]:
+#                 return nums1[nums1_ptr]
+#             if nums1[nums1_ptr] > nums2[nums2_ptr]:
+#                 nums2_ptr+=1
+#             else:
+#                 nums1_ptr+=1
+#         return -1
