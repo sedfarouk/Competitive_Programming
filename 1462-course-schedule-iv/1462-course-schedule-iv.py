293576bd2c230340b80c0e7c63db1a52f8446697
Farouk Sedick <sadikalhanssah@gmail.com>
2024-03-13 11:57:31 +0000

    Time: 1130 ms (20.75%), Space: 20.3 MB (66.44%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.py
+++ 1462-course-schedule-iv/1462-course-schedule-iv.py
@@ -1,22 +1,18 @@
 class Solution:
-    def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
-        graph = [[float('inf')] * numCourses for _ in range(numCourses)]
+    def checkIfPrerequisite(self, numCourses: int, preq: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        dp = [[False for _ in range(numCourses)] for _ in range(numCourses)]
 
-        for a, b in prerequisites:
-            graph[a][b] = 1
-
-        for i in range(numCourses):
-            graph[i][i] = 0
-        
+        for p in preq:
+            dp[p[0]][p[1]] = True
+      
         for k in range(numCourses):
             for i in range(numCourses):
                 for j in range(numCourses):
-                    graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+                    dp[i][j] = dp[i][j] or (dp[i][k] and dp[k][j])
         
-        ans = [False] * len(queries)
-        i = 0
-        for u, v in queries:
-            if graph[u][v] != float('inf'):
-                ans[i] = True
-            i += 1
+        ans = []
+        for q in queries:
+            ans.append(dp[q[0]][q[1]])
         return ans
+        
+        
