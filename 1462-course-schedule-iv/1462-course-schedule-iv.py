a46652238908ae4ecf88d32f400c3c6ee5094942
Farouk Sedick <sadikalhanssah@gmail.com>
2024-03-13 11:57:05 +0000

    Time: 2565 ms (5.10%), Space: 20.1 MB (81.86%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.py
+++ 1462-course-schedule-iv/1462-course-schedule-iv.py
@@ -1,18 +1,22 @@
 class Solution:
-    def checkIfPrerequisite(self, numCourses: int, preq: List[List[int]], queries: List[List[int]]) -> List[bool]:
-        dp = [[False for _ in range(numCourses)] for _ in range(numCourses)]
+    def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        graph = [[float('inf')] * numCourses for _ in range(numCourses)]
 
-        for p in preq:
-            dp[p[0]][p[1]] = True
-      
+        for a, b in prerequisites:
+            graph[a][b] = 1
+
+        for i in range(numCourses):
+            graph[i][i] = 0
+        
         for k in range(numCourses):
             for i in range(numCourses):
                 for j in range(numCourses):
-                    dp[i][j] = dp[i][j] or (dp[i][k] and dp[k][j])
+                    graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
         
-        ans = []
-        for q in queries:
-            ans.append(dp[q[0]][q[1]])
+        ans = [False] * len(queries)
+        i = 0
+        for u, v in queries:
+            if graph[u][v] != float('inf'):
+                ans[i] = True
+            i += 1
         return ans
-        
-        
