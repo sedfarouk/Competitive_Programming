1959af5ffb64d6b042ce8cdcaf21e6ba956b47e4
Farouk Sedick <sadikalhanssah@gmail.com>
2024-03-22 16:22:29 +0000

    Update 2156-find-substring-with-given-hash-value.py

--- 2156-find-substring-with-given-hash-value/2156-find-substring-with-given-hash-value.py
+++ 2156-find-substring-with-given-hash-value/2156-find-substring-with-given-hash-value.py
@@ -1,13 +1,11 @@
 class Solution:
     def subStrHash(self, s: str, power: int, modulo: int, k: int, hashValue: int) -> str:
-        def get_ascii(ch):
-            return ord(ch)-ord('a')+1
-        
         s = s[::-1]
         s_hash, n = 0, len(s)
+        p = pow(power, k-1, modulo)
 
         for i in range(k):
-            s_hash += get_ascii(s[i])*pow(power, k-i-1, modulo)
+            s_hash += (ord(s[i])-96)*pow(power, k-i-1, modulo)
             s_hash %= modulo
 
         ans = ""
@@ -15,10 +13,9 @@ class Solution:
             ans = s[i-k+1:i+1]
 
         for i in range(k, n):
-            s_hash -= get_ascii(s[i-k])*pow(power, k-1, modulo)
+            s_hash -= (ord(s[i-k])-96)*p
             s_hash *= power
-            s_hash += get_ascii(s[i])
-            s_hash %= modulo
+            s_hash += (ord(s[i])-96) % modulo
             
             if s_hash % modulo == hashValue:
                 ans = s[i-k+1:i+1]
