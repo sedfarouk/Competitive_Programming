a631877ad91304078d8c1cedda51e79d8c6bc081
Farouk Sedick <sadikalhanssah@gmail.com>
2024-03-22 10:26:57 +0000

    Time: 42 ms (12.12%), Space: 16.7 MB (10.48%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,10 +1,44 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        l = len(needle)
-        if needle==haystack:
+        MOD = (10**9)+7
+        n, m = len(haystack), len(needle)
+        
+        def get_ascii(letter):
+            return ord(letter)-ord('a')+1
+
+        def check_equal(a, b):
+            for x, y in zip(a, b):
+                if x!=y:
+                    return False
+            return True
+        
+        def string_hash(s, l):
+            hash_val = 0
+            for i in range(l+1):
+                hash_val += ((29**(l-i))*get_ascii(s[i]))
+            return hash_val % MOD
+
+        if n < m:
+            return -1
+        
+        haystack_hash = string_hash(haystack, m-1)
+        needle_hash = string_hash(needle, m-1)
+        
+        if haystack_hash == needle_hash:
             return 0
-        for i in range(len(haystack)):
-            if haystack[i]==needle[0]:
-                if haystack[i:i+l]==needle[:l]:
-                    return i
+
+        for i in range(m, n):
+            haystack_hash -=  get_ascii(haystack[i-m])*(29**(m-1))
+            haystack_hash *= 29
+            haystack_hash += get_ascii(haystack[i])
+            haystack_hash %= MOD
+            
+            if haystack_hash == needle_hash and check_equal(haystack[i-m+1:i+1], needle):
+                return i-m+1
         return -1
+            
+        
+        
+        
+        
+        
