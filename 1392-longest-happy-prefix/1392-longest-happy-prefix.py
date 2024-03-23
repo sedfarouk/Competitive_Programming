3de81572d28f20145ddd6daf12efd0ccbd25f7bd
Farouk Sedick <sadikalhanssah@gmail.com>
2024-03-23 01:11:12 +0000

    Time: 891 ms (25.79%), Space: 18 MB (73.50%) - LeetHub

--- 1392-longest-happy-prefix/1392-longest-happy-prefix.py
+++ 1392-longest-happy-prefix/1392-longest-happy-prefix.py
@@ -1,24 +1,14 @@
 class Solution:
     def longestPrefix(self, s: str) -> str:
         MOD, n = (10**9)+7, len(s)
-        hashes = [0]*n
         ans = ""
 
-        prefix_hash = 0
+        prefix_hash = suffix_hash = 0
         for i in range(n-1):
-            prefix_hash *= 29
-            prefix_hash += (ord(s[i])-96) 
-            prefix_hash %= MOD
-            hashes[i] = prefix_hash
+            prefix_hash = ((prefix_hash*29)+ord(s[i])-96)%MOD
+            suffix_hash = (suffix_hash+(ord(s[-i-1])-96)*pow(29,i,MOD))%MOD
 
-        s = s[::-1]
-        suffix_hash = 0
-        for i in range(n):
-            suffix_hash += (ord(s[i])-96)*pow(29, i, MOD)
-            suffix_hash %= MOD
-            suffix = s[:i+1]
-            
-            if suffix_hash==hashes[i] and len(suffix) > len(ans):
-                ans = suffix
+            if suffix_hash==prefix_hash:
+                ans = s[:i+1]
         
-        return ans[::-1]
+        return ans
