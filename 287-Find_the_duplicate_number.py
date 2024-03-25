3c1ee09f4e6b4f793a14fd2be8bc3cc6b382318e
Farouk Sedick <sadikalhanssah@gmail.com>
2024-03-25 11:43:20 +0000

    Update 287-Find_the_duplicate_number.py

--- 287-Find_the_duplicate_number.py
+++ 287-Find_the_duplicate_number.py
@@ -1,27 +1,40 @@
-# 287. Find the duplicate number - Leetcode
-
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        # temp = [0] * (max(nums)+1)
+        # APPROACH 1
+        slow = fast = nums[0]
 
-        # for i in nums:
-        #     if temp[i]==0:
-        #         temp[i]+=1
-        #     else:
-        #         return i
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
 
-        i = 0
-        n = len(nums)
+            if slow==fast:
+                break
+
+        slow = nums[0]
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[fast]
+        return slow
+
+        # APPROACH 2
+        # bit_manip = 0
+        # for num in nums:
+        #     if (bit_manip&(1<<num))!=0:
+        #         return num
+        #     bit_manip |= (1<<num)
+
+        # APPROACH 3
+        # i = 0
+        # n = len(nums)
         
-        while i < n:
-            correctIdx = nums[i]
-            if nums[i] != nums[correctIdx-1]:
-                nums[i], nums[correctIdx-1] = nums[correctIdx-1], nums[i]
+        # while i < n:
+        #     correctIdx = nums[i]
+        #     if nums[i] != nums[correctIdx-1]:
+        #         nums[i], nums[correctIdx-1] = nums[correctIdx-1], nums[i]
                 
-            else:
-                i += 1
+        #     else:
+        #         i += 1
                 
-        for j in range(n):
-            if j != nums[j]-1:
-                return nums[j]
-
+        # for j in range(n):
+        #     if j != nums[j]-1:
+        #         return nums[j]
