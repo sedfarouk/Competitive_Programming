2b78e9f0e770e0a7c8931351b2febb8647d1d7a9
Farouk Sedick <sadikalhanssah@gmail.com>
2024-09-13 09:47:18 +0000

    Time: 67 ms (80.22%), Space: 18 MB (79.08%) - LeetHub

--- 1414-shortest-path-in-a-grid-with-obstacles-elimination/1414-shortest-path-in-a-grid-with-obstacles-elimination.py
+++ 1414-shortest-path-in-a-grid-with-obstacles-elimination/1414-shortest-path-in-a-grid-with-obstacles-elimination.py
@@ -3,28 +3,33 @@ class Solution:
         n, m = len(grid), len(grid[0])
         dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 
+        if k>=m+n-2:
+            return m+n-2 
+
         def inbounds(r, c):
             return 0 <= r < n and 0 <= c < m
 
-        queue = deque([(0, 0, 0, k)])
-        visited = set()
+        queue = deque([(0, 0, k)])
+        visited = set((0, 0, k))
+        cnt = 0
         while queue:
-            row, col, cnt, q = queue.popleft()
-            if row==n-1 and col==m-1:
-                return cnt
+            size = len(queue)
+            for _ in range(size):
+                row, col, q = queue.popleft()
+                if row==n-1 and col==m-1:
+                    return cnt
 
-            if (row, col, q) in visited:
-                continue
-            visited.add((row, col, q))
+                for dr, dc in dirs:
+                    newr, newc = row + dr, col + dc
+                    if inbounds(newr, newc):
+                        if grid[newr][newc]==0 and (newr, newc, q) not in visited:
+                            queue.append((newr, newc, q))
+                            visited.add((newr, newc, q))
 
-            for dr, dc in dirs:
-                newr, newc = row + dr, col + dc
-                if inbounds(newr, newc):
-                    if grid[newr][newc]==0:
-                        queue.append((newr, newc, cnt+1, q))
-
-                    elif grid[newr][newc]==1 and q > 0:
-                        queue.append((newr, newc, cnt+1, q-1))
+                        elif grid[newr][newc]==1 and q > 0 and (newr, newc, q-1) not in visited:
+                            queue.append((newr, newc, q-1))
+                            visited.add((newr, newc, q-1))
+            cnt += 1
         return -1
 
         
