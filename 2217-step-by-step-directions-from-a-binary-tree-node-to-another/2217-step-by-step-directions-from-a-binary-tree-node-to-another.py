2c8b714e175acfe6d9ca563db9124111de616faf
Farouk Sedick <sadikalhanssah@gmail.com>
2024-09-13 10:59:45 +0000

    Time: 255 ms (73.78%), Space: 55 MB (26.51%) - LeetHub

---
+++ 2217-step-by-step-directions-from-a-binary-tree-node-to-another/2217-step-by-step-directions-from-a-binary-tree-node-to-another.py
@@ -0,0 +1,49 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def getDirections(self, root: Optional[TreeNode], startValue: int, destValue: int) -> str:
+        def dfs(node, val, ans):
+            nonlocal p
+            if node.val==val:
+                p.append(ans[::-1])
+                return
+            
+            if not node.left and not node.right:
+                return None
+
+            res = None
+            if node.left:
+                ans.append("L")
+                res = res or dfs(node.left, val, ans)
+                ans.pop()
+            if node.right:
+                ans.append("R")
+                res = res or dfs(node.right, val, ans)
+                ans.pop()
+            return res 
+        
+        p = []
+        dfs(root, startValue, [])
+        dfs(root, destValue, [])
+        pathStart = p[0][::-1]
+        pathDest = p[1][::-1]
+
+        if pathStart==None:
+            return pathDest
+
+        j = 0
+        for i in range(min(len(pathStart), len(pathDest))):
+            if pathStart[i]!=pathDest[i]:
+                break
+            j += 1
+        pathDest = pathDest[j:]
+        pathStart = pathStart[j:]
+
+        return "U"*len(pathStart) + "".join(pathDest)
+
+        
+        
