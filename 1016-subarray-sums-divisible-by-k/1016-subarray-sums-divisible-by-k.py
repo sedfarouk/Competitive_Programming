b15f0d318c50e59311a154824ebfd2f03f771134
Farouk Sedick <sadikalhanssah@gmail.com>
2024-09-23 14:21:05 +0000

    Time: 229 ms (66.12%), Space: 21.4 MB (65.2%) - LeetHub

--- 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.py
+++ 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.py
@@ -2,13 +2,13 @@ class Solution:
     def subarraysDivByK(self, nums: List[int], k: int) -> int:
         hashmap = defaultdict(int)
         hashmap[0] = 1
-        ans, cnt =  0, 0
-
-        for i in range(len(nums)):
-            cnt+=nums[i]
-
+        ans = 0
+        cnt = 0
+        
+        for num in nums:
+            cnt += num
             ans += hashmap[cnt % k]
             hashmap[cnt % k] += 1
+        
         return ans
-
             
