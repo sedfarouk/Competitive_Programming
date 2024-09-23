226332b830783adfd909ff8359b66bbb6afb2057
Farouk Sedick <sadikalhanssah@gmail.com>
2024-09-23 14:58:27 +0000

    Time: 445 ms (12.23%), Space: 22.2 MB (39.42%) - LeetHub

--- 1473-find-the-longest-substring-containing-vowels-in-even-counts/1473-find-the-longest-substring-containing-vowels-in-even-counts.py
+++ 1473-find-the-longest-substring-containing-vowels-in-even-counts/1473-find-the-longest-substring-containing-vowels-in-even-counts.py
@@ -1,18 +1,26 @@
 class Solution:
     def findTheLongestSubstring(self, s: str) -> int:
         hashmap = defaultdict(int)
-        cnt = (0, 0, 0, 0, 0)
+        cnt = (2**5)-1
         hashmap[cnt] = 0
         ans = 0
 
         for r in range(len(s)):
-            cnt = (cnt[0]+(s[r]=='a'), cnt[1]+(s[r]=='e'), cnt[2]+(s[r]=='i'), cnt[3]+(s[r]=='o'), cnt[4]+(s[r]=='u'))
-            exp = (cnt[0]%2, cnt[1]%2, cnt[2]%2, cnt[3]%2, cnt[4]%2) 
+            if s[r]=='a':
+                cnt ^= 1
+            elif s[r]=='e':
+                cnt ^= 2
+            elif s[r]=='i':
+                cnt ^= 4
+            elif s[r]=='o':
+                cnt ^= 8
+            elif s[r]=='u':
+                cnt ^= 16
 
-            if exp in hashmap:
-                ans = max(ans, r-hashmap[exp]+1)
+            if cnt in hashmap:
+                ans = max(ans, r-hashmap[cnt]+1)
             else:
-                hashmap[exp] = r+1
+                hashmap[cnt] = r+1
         
         return ans
         
