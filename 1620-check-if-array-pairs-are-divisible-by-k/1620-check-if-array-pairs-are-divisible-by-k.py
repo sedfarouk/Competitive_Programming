0dc19edae6493b8b2768bcf604a1f17823cadaae
Farouk Sedick <sadikalhanssah@gmail.com>
2024-10-01 10:12:25 +0000

    Time: 497 ms (64.06%), Space: 30.5 MB (27.88%) - LeetHub

--- 1620-check-if-array-pairs-are-divisible-by-k/1620-check-if-array-pairs-are-divisible-by-k.py
+++ 1620-check-if-array-pairs-are-divisible-by-k/1620-check-if-array-pairs-are-divisible-by-k.py
@@ -2,14 +2,11 @@ class Solution:
     def canArrange(self, arr: List[int], k: int) -> bool:
         mods = Counter([x % k for x in arr])
 
-        for num in arr:
-            if mods[num % k] == 0:
-                continue
-
-            mods[num % k] -= 1
-            if mods[(k-(num % k)) % k] == 0:
+        for num in mods:
+            if num==0 and mods[num] % 2 != 0:
                 return False
-                
-            mods[(k-(num % k)) % k] -= 1
+            rem = (k-num) % k
+            if mods[num] != mods[rem]:
+                return False
         
         return True
