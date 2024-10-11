6c6050c315ffeb7ee1f03e57d28bc371145c2052
Farouk Sedick <sadikalhanssah@gmail.com>
2024-10-11 23:58:05 +0000

    Time: 1842 ms (10.42%), Space: 43 MB (5.65%) - LeetHub

--- 2054-the-number-of-the-smallest-unoccupied-chair/2054-the-number-of-the-smallest-unoccupied-chair.py
+++ 2054-the-number-of-the-smallest-unoccupied-chair/2054-the-number-of-the-smallest-unoccupied-chair.py
@@ -1,16 +1,18 @@
 class Solution:
     def smallestChair(self, times: List[List[int]], targetFriend: int) -> int:
-
-        order = sorted(range(len(times)), key = lambda x: times[x][0])  # <-- 1)
-        emptySeats, takenSeats = list(range(len(times))), []            # <-- 2)
-
-        for i in order:                                                 # <-- 3)
-            ar, lv = times[i]
-
-            while takenSeats and takenSeats[0][0] <= ar:
-                heappush(emptySeats, heappop(takenSeats)[1])
-            seat = heappop(emptySeats)                                  # <-- 4)
-
-            if i == targetFriend: return seat
-
-            heappush(takenSeats,(lv, seat))                             # <-- 5)
+        opening  = {times[i][0]:i for i in range(len(times))}
+        closing = defaultdict(list)
+        assign = {}
+        heap = [i for i in range(10**5 + 1)]
+        
+        for i in range(len(times)):
+            closing[times[i][1]].append(i)
+        
+        for i in range(10**5 + 1):
+            while closing[i]:
+                heappush(heap, assign[closing[i].pop()])
+            
+            if i in opening:
+                assign[opening[i]] = heappop(heap)
+        
+        return assign[targetFriend]
