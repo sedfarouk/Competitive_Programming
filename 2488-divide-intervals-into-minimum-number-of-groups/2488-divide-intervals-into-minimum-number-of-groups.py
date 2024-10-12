302001442824a5480f12bec84c40d6ac9269b175
Farouk Sedick <sadikalhanssah@gmail.com>
2024-10-12 01:10:28 +0000

    Time: 1078 ms (72.36%), Space: 56.4 MB (49.16%) - LeetHub

--- 2488-divide-intervals-into-minimum-number-of-groups/2488-divide-intervals-into-minimum-number-of-groups.py
+++ 2488-divide-intervals-into-minimum-number-of-groups/2488-divide-intervals-into-minimum-number-of-groups.py
@@ -1,25 +1,25 @@
 class Solution:
     def minGroups(self, intervals: List[List[int]]) -> int:
         # DIFFERENCE ARRAY (SWEEP LINE)
-        arr = []
+        # arr = []
 
-        for r, l in intervals:
-            arr.append((r, 1))
-            arr.append((l+1, -1))
+        # for r, l in intervals:
+        #     arr.append((r, 1))
+        #     arr.append((l+1, -1))
 
-        ans = curr = 0
-        for x, diff in sorted(arr):
-            curr += diff
-            ans = max(ans, curr)
-        return ans
+        # ans = curr = 0
+        # for x, diff in sorted(arr):
+        #     curr += diff
+        #     ans = max(ans, curr)
+        # return ans
 
         # HEAP
 
-        # intervals.sort()
-        # heap = [intervals[0][1]]
+        intervals.sort()
+        heap = [intervals[0][1]]
 
-        # for r, l in intervals[1:]:
-        #     if r > heap[0]:
-        #         heappop(heap)
-        #     heappush(heap, l)
-        # return len(heap)
+        for r, l in intervals[1:]:
+            if r > heap[0]:
+                heappop(heap)
+            heappush(heap, l)
+        return len(heap)
