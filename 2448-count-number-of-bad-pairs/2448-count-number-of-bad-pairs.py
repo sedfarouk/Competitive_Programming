ce4f88dc6dbd1f1ea1ea3f39faad736afb1ab7ae
Farouk Sedick <sadikalhanssah@gmail.com>
2025-02-12 15:23:08 +0000

    Time: 79 ms (56.57%), Space: 39 MB (25.69%) - LeetHub

---
+++ 2448-count-number-of-bad-pairs/2448-count-number-of-bad-pairs.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countBadPairs(self, nums: List[int]) -> int:
+        n = len(nums)
+        hashmap = defaultdict(int)
+        ans = 0
+
+        for i in range(n):
+            diff = nums[i] - i
+            ans += hashmap[diff]
+            hashmap[diff] += 1
+        
+        return n * (n - 1) // 2 - ans
