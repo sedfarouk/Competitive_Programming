b1015390a52a0682832f36a2c00ede8c236580e5
Farouk Sedick <sadikalhanssah@gmail.com>
2025-06-02 10:33:41 +0000

    Time: 11 ms (74.29%), Space: 18.2 MB (82.48%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -1,46 +1,39 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        def isSafe(mat, row, col):
-            x, y = row, col
-            while x >= 0:
-                if mat[x][y]=='Q':
-                    return False
-                x-=1
-
-            x, y = row, col
-            while x >= 0 and y < n:
-                if mat[x][y]=='Q':
-                    return False
-                x-=1;y+=1
-
-            x, y = row, col
-            while x >= 0 and y >= 0:
-                if mat[x][y]=='Q':
-                    return False
-                x-=1; y-=1
-
-            return True            
-
-        def backtrack(mat, row):
-            if row == n:
-                cpy = []
-                
-                for row in mat:
-                    line = "".join(row)
-                    cpy.append(line)
-                solutions.append(cpy)
-                return
-
-            for col in range(n):
-                if isSafe(mat, row, col):
-                    mat[row][col] = 'Q'
-                    backtrack(mat, row+1)
-                    mat[row][col] = '.'
-
         board = [['.'] * n for _ in range(n)]
+        cols, posDiag, negDiag = set(), set(), set()
+        # directions = [(-1, 1), (1, 1), (1, -1), (-1, -1), (1, 0), (0, 1), (-1, 0), (0, -1)]
+
+        def valid(i, j):
+            return j not in cols and (i + j) not in posDiag and (i - j) not in negDiag
+        #     for dx, dy in directions:
+        #         r, c = i, j
+
+        #         while 0 <= r < n and 0 <= c < n:
+        #             if board[r][c] == 'Q': return False
+        #             r += dx; c += dy
+
+        #     return True 
+
+        def backtrack(i):
+            if i == n: 
+                solutions.append(["".join(row) for row in board])
+                return 
+
+            for j in range(n):
+                if valid(i, j):
+                    board[i][j] = 'Q'
+                    cols.add(j)
+                    posDiag.add(i + j); negDiag.add(i - j)
+
+                    backtrack(i + 1)
+
+                    board[i][j] = '.'
+                    posDiag.remove(i + j); negDiag.remove(i - j)
+                    cols.remove(j)
+
         solutions = []
-        backtrack(board, 0)
+        backtrack(0)
         return solutions
 
-
-    
+            
