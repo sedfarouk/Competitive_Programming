302cec9da6474684c00de8c13c13ab6d545785af
Farouk Sedick <sadikalhanssah@gmail.com>
2025-10-10 08:36:33 +0000

    Time: 1177 ms (46.25%) | Memory: 31.3 MB (36.88%) - LeetSync

--- 3383-taking-maximum-energy-from-the-mystic-dungeon/taking-maximum-energy-from-the-mystic-dungeon.py
+++ 3383-taking-maximum-energy-from-the-mystic-dungeon/taking-maximum-energy-from-the-mystic-dungeon.py
@@ -2,7 +2,10 @@ class Solution:
     def maximumEnergy(self, energy: List[int], k: int) -> int:
         ans = float("-inf")
         n = len(energy)
-        
+
+        #Time complexity: O(nlogk) ~ due to harmonic series. 
+        # inner loop runs sum(n / k) for each k 
+
         for i in range(n - 1, n - k - 1, -1):
             curr = 0
             end = i
