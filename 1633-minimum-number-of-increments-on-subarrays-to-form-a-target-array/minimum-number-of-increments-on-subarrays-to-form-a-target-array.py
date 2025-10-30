cb11efafa0075c223e06712e00ea5532b11eaa79
Farouk Sedick <sadikalhanssah@gmail.com>
2025-10-30 13:33:46 +0000

    Time: 74 ms (32.73%) | Memory: 26.8 MB (19.19%) - LeetSync

--- 1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array/minimum-number-of-increments-on-subarrays-to-form-a-target-array.py
+++ 1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array/minimum-number-of-increments-on-subarrays-to-form-a-target-array.py
@@ -1,26 +1,35 @@
 class Solution:
     def minNumberOperations(self, target: List[int]) -> int:
         n = len(target)
-        stack = []
-        prevSmaller = [-1] * n
-        for i in range(n - 1, -1, -1):
-            while stack and target[stack[-1]] >= target[i]:
-                prevSmaller[stack.pop()] = i
-            stack.append(i)
-
-        stack = []
-        prevGreater = [-1] * n
-        for i in range(n - 1, -1, -1):
-            while stack and target[stack[-1]] < target[i]:
-                prevGreater[stack.pop()] = i
-            stack.append(i)
-
         ans = target[0]
-        for i in range(1, n):
-            if prevSmaller[i] > prevGreater[i]:
-                ans += target[i] - target[prevSmaller[i]]
 
-        return ans 
+        # COMPARE CONSECUTIVE ELEMENTS
+        for i in range(1, n):
+            ans += max(0, target[i] - target[i - 1])
+        return ans
+
+        # MONOTONIC STACKS
+
+        # stack = []
+        # prevSmaller = [-1] * n
+        # for i in range(n - 1, -1, -1):
+        #     while stack and target[stack[-1]] >= target[i]:
+        #         prevSmaller[stack.pop()] = i
+        #     stack.append(i)
+
+        # stack = []
+        # prevGreater = [-1] * n
+        # for i in range(n - 1, -1, -1):
+        #     while stack and target[stack[-1]] < target[i]:
+        #         prevGreater[stack.pop()] = i
+        #     stack.append(i)
+
+        # ans = target[0]
+        # for i in range(1, n):
+        #     if prevSmaller[i] > prevGreater[i]:
+        #         ans += target[i] - target[prevSmaller[i]]
+
+        # return ans 
 
         
 
