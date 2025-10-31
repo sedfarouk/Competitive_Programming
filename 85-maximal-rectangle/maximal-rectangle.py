f55044c3dac4f269a6acf2cfed7afefa952b795d
Farouk Sedick <sadikalhanssah@gmail.com>
2025-10-31 09:22:47 +0000

    Time: 2266 ms (5.02%) | Memory: 21.8 MB (99.39%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.py
+++ 85-maximal-rectangle/maximal-rectangle.py
@@ -1,35 +1,45 @@
 class Solution:
     def maximalRectangle(self, matrix: List[List[str]]) -> int:
-        # MONOTONIC STACKS
+        n, m = len(matrix), len(matrix[0])
+        ans = 0
+
+        # MONOTONIC STACKS TC: O(N * M), SC: O(M)
         # Builds on Largest Rectangle in Histogram problem (coincidentally the previous problem before this, lol)
         # Combination of 2 techniques; Row-wise cumulative sum && Monotonic stacks
 
-        n, m = len(matrix), len(matrix[0])
+        # curr = [0] * m
+        # for row in matrix:
+        #     for i in range(m):
+        #         if row[i] != '0': 
+        #             curr[i] += int(row[i]) 
+        #         else:
+        #             curr[i] = 0
 
-        ans = 0
-        curr = [0] * m
-        for row in matrix:
-            for i in range(m):
-                if row[i] != '0': 
-                    curr[i] += int(row[i]) 
-                else:
-                    curr[i] = 0
+        #     stack = []
+        #     nextSmaller = [m] * m
+        #     prevSmaller = [-1] * m
 
-            stack = []
-            nextSmaller = [m] * m
-            prevSmaller = [-1] * m
+        #     for i in range(m):
+        #         while stack and curr[stack[-1]] > curr[i]:
+        #             nextSmaller[stack.pop()] = i
 
-            for i in range(m):
-                while stack and curr[stack[-1]] > curr[i]:
-                    nextSmaller[stack.pop()] = i
+        #         if stack:
+        #             prevSmaller[i] = stack[-1]
 
-                if stack:
-                    prevSmaller[i] = stack[-1]
+        #         stack.append(i)
 
-                stack.append(i)
+        #     for i in range(m):
+        #         ans = max(ans, curr[i] * (nextSmaller[i] - prevSmaller[i] - 1))
 
-            for i in range(m):
-                ans = max(ans, curr[i] * (nextSmaller[i] - prevSmaller[i] - 1))
+        # DYNAMIC PROGRAMMING TC: O(N^2 * M); SC: O(1)
+        for i in range(n):
+            for j in range(m):
+                matrix[i][j] = int(matrix[i][j]) + (matrix[i][j - 1] if (j > 0 and matrix[i][j] != '0') else 0)
+
+                max_width = matrix[i][j]
+                for k in range(i, -1, -1):
+                    max_width = min(max_width, matrix[k][j])
+                    ans = max(ans, max_width * (i - k + 1))
 
         return ans
             
