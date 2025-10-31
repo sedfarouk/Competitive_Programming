4ea9b284b3ed98c34813140abc0e2a45ca523c21
Farouk Sedick <sadikalhanssah@gmail.com>
2025-10-31 10:16:10 +0000

    Time: 295 ms (51.79%) | Memory: 22 MB (17.14%) - LeetSync

---
+++ 1466-jump-game-v/jump-game-v.py
@@ -0,0 +1,26 @@
+class Solution:
+    def maxJumps(self, arr: List[int], d: int) -> int:
+        @lru_cache(None)
+        def dp(idx):
+            ans = 0
+            for i in range(idx - 1, max(-1, idx - d - 1), -1):
+                if arr[idx] > arr[i]:
+                    ans = max(ans, dp(i) + 1)
+                else:
+                    break
+
+            for i in range(idx + 1, min(len(arr), idx + d + 1)):
+                if arr[idx] > arr[i]:
+                    ans = max(ans, dp(i) + 1)
+                else:
+                    break
+
+            return ans
+
+        res = 0
+        for i in range(len(arr)):
+            res = max(res, dp(i) + 1)
+            print(res)
+
+        return res
+        
