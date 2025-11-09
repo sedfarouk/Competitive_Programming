683945e6e610e29205644bc99b009bea8be85c68
Farouk Sedick <sadikalhanssah@gmail.com>
2025-11-09 07:58:49 +0000

    Time: 0 ms (100.00%) | Memory: 17.7 MB (50.48%) - LeetSync

--- 2288-count-operations-to-obtain-zero/count-operations-to-obtain-zero.py
+++ 2288-count-operations-to-obtain-zero/count-operations-to-obtain-zero.py
@@ -1,9 +1,8 @@
 class Solution:
     def countOperations(self, num1: int, num2: int) -> int:
         def f(a, b):
-            if not a or not b:
+            if not b:
                 return 0
-            if a < b: a, b = b, a
-            return f(a - b, b) + 1
+            return f(b, a % b) + a // b
         return f(num1, num2)
             
