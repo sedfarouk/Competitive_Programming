4f5deebf39571f61b59a6db98d397deb1ff555e8
Farouk Sedick <sadikalhanssah@gmail.com>
2025-11-10 15:12:30 +0000

    Time: 20 ms (61.31%) | Memory: 28.3 MB (9.52%) - LeetSync

--- 1424-maximum-candies-you-can-get-from-boxes/maximum-candies-you-can-get-from-boxes.py
+++ 1424-maximum-candies-you-can-get-from-boxes/maximum-candies-you-can-get-from-boxes.py
@@ -2,19 +2,44 @@ class Solution:
     def maxCandies(self, status: List[int], candies: List[int], keys: List[List[int]], containedBoxes: List[List[int]], initialBoxes: List[int]) -> int:
         n = len(status)
         currKeys = set([i for i in range(n) if status[i]])
-        currBoxes = set(initialBoxes)
+        queue = deque()
+        visitedBoxes, boxesToSearch = set(), set()
         ans = 0
 
-        visitedBoxes = set()
-        while True:
-            oper = False
-            for box in list(currBoxes):
-                if box in currKeys:
-                    oper = True
-                    ans += candies[box]
-                    currKeys |= set(keys[box])
-                    currBoxes |= set(containedBoxes[box]) - visitedBoxes
-                    currBoxes.remove(box)
-                    visitedBoxes.add(box)
+        for initialBox in initialBoxes:
+            if status[initialBox]:
+                queue.append(initialBox)
+                visitedBoxes.add(initialBox)
+            else:
+                boxesToSearch.add(initialBox)
 
-            if not oper: return ans
+        while queue:
+            q = queue.popleft()
+            ans += candies[q]
+
+            for key in keys[q]:
+                if key not in visitedBoxes:
+                    if key in boxesToSearch:
+                        queue.append(key)
+                        boxesToSearch.remove(key)
+                        visitedBoxes.add(key)
+                    else:
+                        currKeys.add(key)
+
+            for box in containedBoxes[q]:
+                if box not in visitedBoxes:
+                    if box in currKeys:
+                        queue.append(box)
+                        visitedBoxes.add(box)
+                        
+                    else:
+                        boxesToSearch.add(box)
+
+        return ans
+
+
+
+
+
+
+            
