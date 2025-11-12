0aaecb8c2d707c7184cc8a58e3bc4b2b5a086a3e
Farouk Sedick <sadikalhanssah@gmail.com>
2025-11-12 21:54:52 +0000

    Time: 1202 ms (56.95%) | Memory: 19 MB (77.48%) - LeetSync

---
+++ 1680-count-all-possible-routes/count-all-possible-routes.py
@@ -0,0 +1,17 @@
+class Solution:
+    def countRoutes(self, locations: List[int], start: int, finish: int, fuel: int) -> int:
+        n = len(locations)
+        MOD = 10 ** 9 + 7
+
+        @lru_cache(None)
+        def dp(idx, f):
+            ans = int(idx == finish)
+            for i in range(n):
+                if i != idx and f - abs(locations[i] - locations[idx]) >= 0:
+                    ans = (ans + dp(i, f - abs(locations[i] - locations[idx]))) % MOD
+            
+            return ans
+
+        res = dp(start, fuel)
+        dp.cache_clear()
+        return res
