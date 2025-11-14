ad472ddf96712f1a959b9e44d699a3694149ca46
Farouk Sedick <sadikalhanssah@gmail.com>
2025-11-14 19:38:51 +0000

    Time: 0 ms (100.00%) | Memory: 17.9 MB (37.87%) - LeetSync

--- 726-number-of-atoms/number-of-atoms.py
+++ 726-number-of-atoms/number-of-atoms.py
@@ -8,31 +8,39 @@ class Solution:
         while i < n:
             curr = ''
 
+            # START CALCULATING VALUES FOR THOSE IN CURRENT BRACKET
             if formula[i] == '(':
                 stk.append(defaultdict(int))
                 i += 1
 
+            # GET CURRENT ELEMENR
             while i < n and formula[i].isalpha():
                 if curr and formula[i].isupper():
                     break
                 curr += formula[i]
                 i += 1
 
+            # INDICATES IF WE ARE DONE CALCULATING FOR ALL ELEMENTS IN A BRACKET
             pop = False
             if i < n and formula[i] == ')':
                 pop = True
                 i += 1
 
+            # GET CURRENT ELEMENT/BRACKET MULTIPLICATION FACTOR
             cnt = 0
             while i < n and formula[i].isdigit():
                 cnt = (cnt * 10) + int(formula[i])
                 i += 1
             cnt = max(1, cnt)
 
+            # IF A BRACKET JUST GOT CLOSED THEN MULTIPLY EACH ELEMENT IN BRACKET WITH MULTIPLICATION FACTOR
+            # AND INCREASE INITIAL VALUES OF SUCH ELEMENTS IN PREVIOUS BRACKET
             if pop:
                 col = stk.pop()
                 for k, v in col.items():
                     stk[-1][k] += v * cnt
+
+            # ASSIGN CURRENT ELEMENT ITS MULTIPLICATION FACTOR
             if curr: stk[-1][curr] += cnt
 
         ans = ''
