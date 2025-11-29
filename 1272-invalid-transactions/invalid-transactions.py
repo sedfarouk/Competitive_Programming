9e822e8630a61c31ecd9ac721aff160e2b398e20
Farouk Sedick <sadikalhanssah@gmail.com>
2025-11-29 16:09:08 +0000

    Time: 27 ms (49.03%) | Memory: 18.7 MB (29.08%) - LeetSync

--- 1272-invalid-transactions/invalid-transactions.py
+++ 1272-invalid-transactions/invalid-transactions.py
@@ -1,35 +1,35 @@
-from collections import defaultdict, deque
-from typing import List
-
 class Solution:
     def invalidTransactions(self, transactions: List[str]) -> List[str]:
         n = len(transactions)
         parsed = []
-        for i, t in enumerate(transactions):
-            name, time, amt, city = t.split(',')
-            parsed.append((name, int(time), int(amt), city, i))
 
-        parsed.sort(key=lambda x: x[1])
+        for idx, transaction in enumerate(transactions):
+            name, time, amt, city = transaction.split(",")
+            parsed.append((name, int(time), int(amt), city, idx))
 
-        tr = defaultdict(lambda: defaultdict(deque))
-        invalid = [False] * n  
+        parsed.sort(key = lambda x: x[1])
+        city_trans = defaultdict(lambda: defaultdict(deque))
+        invalid = [False] * n
 
         for name, time, amt, city, idx in parsed:
             if amt > 1000:
                 invalid[idx] = True
 
-            for other_city, name_map in tr.items():
+            for other_city, name_maps in city_trans.items():
                 if other_city == city:
                     continue
 
-                dq = name_map[name]
+                dq = name_maps[name]
+
                 while dq and time - dq[0][0] > 60:
                     dq.popleft()
 
-                for t2, a2, idx2 in dq:
+                if dq: invalid[idx] = True
+
+                for time2, idx2 in dq:
                     invalid[idx2] = True
-                    invalid[idx] = True
 
-            tr[city][name].append((time, amt, idx))
+            city_trans[city][name].append((time, idx))
 
-        return [transactions[i] for i in range(n) if invalid[i]]
+        return [transactions[x] for x in range(n) if invalid[x]]
+
