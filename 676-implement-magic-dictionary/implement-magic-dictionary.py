978bfeacdde13c58c27d34631aa7d327cc7820ac
Farouk Sedick <sadikalhanssah@gmail.com>
2025-11-30 08:48:31 +0000

    Time: 170 ms (16.11%) | Memory: 22.1 MB (7.22%) - LeetSync

--- 676-implement-magic-dictionary/implement-magic-dictionary.py
+++ 676-implement-magic-dictionary/implement-magic-dictionary.py
@@ -1,26 +1,51 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.end = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+        self.cnt = 0
+
+    def add(self, word):
+        curr = self.root
+
+        for ch in word:
+            if ch not in curr.children:
+                curr.children[ch] = TrieNode()
+            curr = curr.children[ch]
+        curr.end = True
+
+    def searchMagic(self, word):
+        def dfs(root, i):
+            if i == len(word):
+                return self.cnt == 1 and root.end
+
+            for ch in root.children:
+                self.cnt += int(word[i] != ch)
+
+                if self.cnt < 2 and dfs(root.children[ch], i + 1):
+                    return True
+
+                self.cnt -= int(word[i] != ch)
+
+            return False
+
+        return dfs(self.root, 0)
+
 class MagicDictionary:
 
     def __init__(self):
-        self.dic = []
+        self.trie = Trie()
 
     def buildDict(self, dictionary: List[str]) -> None:
         for word in dictionary:
-            self.dic.append(word)
+            self.trie.add(word)
 
     def search(self, searchWord: str) -> bool:
-        for w in self.dic:
-            if len(w) != len(searchWord):
-                continue
-
-            cnt = 0
-            for i in range(len(searchWord)):
-                cnt += int(searchWord[i] != w[i])
-
-            if cnt == 1:
-                return True
-        
-        return False
-
+        self.trie.cnt = 0
+        return self.trie.searchMagic(searchWord)
 
 # Your MagicDictionary object will be instantiated and called as such:
 # obj = MagicDictionary()
