3ff2cbbc9f96f13d9544a9d4819a16bc7954d414
Farouk Sedick <sadikalhanssah@gmail.com>
2025-11-30 10:21:57 +0000

    Time: 0 ms (100.00%) | Memory: 17.6 MB (98.18%) - LeetSync

---
+++ 2532-remove-letter-to-equalize-frequency/remove-letter-to-equalize-frequency.py
@@ -0,0 +1,14 @@
+class Solution:
+    def equalFrequency(self, word: str) -> bool:
+        freqs = list(Counter(word).values())
+        u = set(freqs)
+
+        if len(u) > 2:
+            return False
+
+        mn, mx = min(u), max(u)
+        fmn, fmx = freqs.count(mn), freqs.count(mx)
+        if (mn == 1 and (fmn == 1 or fmn == len(freqs))) or (mx - mn == 1 and fmx == 1 and fmn == len(freqs) - 1):
+            return True
+        
+        return len(u) == len(freqs) == 1
