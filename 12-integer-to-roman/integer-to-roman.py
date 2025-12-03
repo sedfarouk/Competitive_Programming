c22e03aa7e1cbe0b168210289ca1e79287f35c11
Farouk Sedick <sadikalhanssah@gmail.com>
2025-12-03 08:01:52 +0000

    Time: 7 ms (43.74%) | Memory: 17.9 MB (44.06%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.py
+++ 12-integer-to-roman/integer-to-roman.py
@@ -1,6 +1,6 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        roman = {1:'I', 5:'V', 10:'X', 50:'L', 100:'C', 500:'D', 1000:'M', 5000:''}
+        roman = {1:'I', 5:'V', 10:'X', 50:'L', 100:'C', 500:'D', 1000:'M'}
         ans = []
 
         i = 0
