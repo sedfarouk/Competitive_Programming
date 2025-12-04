eadc2f7d026c7f1e88148ab7ee1fc03b15199a32
Farouk Sedick <sadikalhanssah@gmail.com>
2025-12-04 18:15:03 +0000

    Time: 187 ms (11.68%) | Memory: 18.5 MB (5.80%) - LeetSync

--- 47-permutations-ii/permutations-ii.py
+++ 47-permutations-ii/permutations-ii.py
@@ -1,17 +1,19 @@
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
         n = len(nums)
+        candidates = set([*range(n)])
 
         def backtrack():
             if len(curr) == n:
                 solutions.add(tuple([nums[i] for i in curr]))
                 return 
 
-            for i in range(n):
-                if i not in curr:
-                    curr.append(i)
-                    backtrack()
-                    curr.pop()
+            for i in list(candidates):
+                candidates.remove(i)
+                curr.append(i)
+                backtrack()
+                curr.pop()
+                candidates.add(i)
 
         solutions, curr = set(), []
         backtrack()
