20d734dc2e3e810338b636ae1ef795693d8e9183
Farouk Sedick <sadikalhanssah@gmail.com>
2025-12-08 23:47:28 +0000

    Time: 11 ms (49.33%) | Memory: 21.6 MB (53.63%) - LeetSync

--- 554-brick-wall/brick-wall.py
+++ 554-brick-wall/brick-wall.py
@@ -1,18 +1,16 @@
 class Solution:
     def leastBricks(self, wall: List[List[int]]) -> int:
         n = len(wall)
-        freqs = defaultdict(int)
-        interceptions = 0
+        freqs = defaultdict(lambda : n)
+        ans = n
 
         for row in wall:
             curr = 0
             for i in range(len(row) - 1):
                 curr += row[i]
-                freqs[curr] += 1
-        
-        for v in freqs.values():
-            interceptions = max(interceptions, v)
+                freqs[curr] -= 1
+                ans = min(ans, freqs[curr])
 
-        return n - interceptions
+        return ans
 
         
