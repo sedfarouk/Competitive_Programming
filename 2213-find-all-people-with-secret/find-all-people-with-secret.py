2eeef1ad7a37ef6f4726c50a341c4a6f647ff41b
Farouk Sedick <sadikalhanssah@gmail.com>
2025-12-20 08:34:41 +0000

    Time: 481 ms (19.13%) | Memory: 84.9 MB (5.55%) - LeetSync

---
+++ 2213-find-all-people-with-secret/find-all-people-with-secret.py
@@ -0,0 +1,27 @@
+class Solution:
+    def findAllPeople(self, n: int, meetings: List[List[int]], firstPerson: int) -> List[int]:
+        graph = defaultdict(list)
+
+        for u, v, t in meetings:
+            graph[u].append((t, v))  
+            graph[v].append((t, u))      
+
+        for node in graph:
+            graph[node].sort()
+
+        vis = set()
+        queue = [(0, 0), (0, firstPerson)]
+        time = { i: float("inf") for i in range(n) }
+        
+        while queue:
+            t1, q = heappop(queue)
+            vis.add(q)
+
+            for t2, nei in graph[q]:
+                if t2 >= t1 and t2 < time[nei] and nei not in vis:
+                    time[nei] = t2
+                    heappush(queue, (t2, nei))
+
+        return list(vis)
+            
+
