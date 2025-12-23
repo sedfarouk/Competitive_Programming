85fc6615b1857a6c7eb6468c61c05f30a80c39af
Farouk Sedick <sadikalhanssah@gmail.com>
2025-12-23 13:44:17 +0000

    Time: 323 ms (44.09%) | Memory: 56.5 MB (96.06%) - LeetSync

--- 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.py
+++ 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.py
@@ -2,18 +2,29 @@ class Solution:
     def maxTwoEvents(self, events: List[List[int]]) -> int:
         n = len(events)
         events.sort()
+        suff = [0]
 
-        @cache
-        def dp(i, p):
-            if i == n:
-                return 0
+        for i in range(n - 1, -1, -1):
+            suff.append(max(suff[-1], events[i][2]))
+        suff.reverse()
 
-            _, end, val = events[i]
+        ans = 0
+        for _, end, val in events:
+            ans = max(ans, val + suff[bisect_right(events, [end, float("inf")])])
+
+        return ans        
+        
+        # @cache
+        # def dp(i, p):
+        #     if i == n:
+        #         return 0
+
+        #     _, end, val = events[i]
             
-            if p:
-                return max(val, dp(i + 1, True))
-            return max(dp(bisect_right(events, [end, float("inf")]), True) + val, dp(i + 1, False))
+        #     if p:
+        #         return max(val, dp(i + 1, True))
+        #     return max(dp(bisect_right(events, [end, float("inf")]), True) + val, dp(i + 1, False))
 
-        res = dp(0, False)
-        dp.cache_clear()
-        return res
+        # res = dp(0, False)
+        # dp.cache_clear()
+        # return res
