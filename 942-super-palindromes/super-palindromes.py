24d091973b962222a3ca7b5d62ddf8c29957b619
Farouk Sedick <sadikalhanssah@gmail.com>
2025-12-25 07:10:49 +0000

    Time: 310 ms (53.76%) | Memory: 17.3 MB (98.92%) - LeetSync

--- 942-super-palindromes/super-palindromes.py
+++ 942-super-palindromes/super-palindromes.py
@@ -1,22 +1,30 @@
 class Solution:
     def superpalindromesInRange(self, left: str, right: str) -> int:
         left, right = int(left), int(right)
-        l, r = int(left ** 0.5), int(right** 0.5)
         ans = 0
 
-        def isSuperPalindrome(num):
-            sq_num = str(int(num) ** 2)
-
-            if left <= int(sq_num) <= right and sq_num == sq_num[::-1]:
-                return True
-            return False
+        def isPalindrome(x):
+            return x == x[::-1]
 
         for i in range(10 ** 5):
             num = str(i)
-            num1, num2 = num + num[:-1][::-1], num + num[::-1]
+            num = num + num[:-1][::-1]
+            sq_num = int(num) ** 2
 
-            ans += int(isSuperPalindrome(num1))
-            ans += int(isSuperPalindrome(num2))
+            if sq_num > right:
+                break
+
+            ans += int(sq_num >= left and isPalindrome(str(sq_num)))
+
+        for i in range(10 ** 5):
+            num = str(i)
+            num = num + num[::-1]
+            sq_num = int(num) ** 2
+
+            if sq_num > right:
+                break
+
+            ans += int(sq_num >= left and isPalindrome(str(sq_num)))
 
         return ans
             
