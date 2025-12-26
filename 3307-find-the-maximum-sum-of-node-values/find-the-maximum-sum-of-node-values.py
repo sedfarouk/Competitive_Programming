2f8fb40682876e619201184a35a5e0ad1fe3c422
Farouk Sedick <sadikalhanssah@gmail.com>
2025-12-26 09:55:19 +0000

    Time: 121 ms (23.40%) | Memory: 26.3 MB (41.49%) - LeetSync

--- 3307-find-the-maximum-sum-of-node-values/find-the-maximum-sum-of-node-values.py
+++ 3307-find-the-maximum-sum-of-node-values/find-the-maximum-sum-of-node-values.py
@@ -1,14 +1,23 @@
 class Solution:
     def maximumValueSum(self, nums: List[int], k: int, edges: List[List[int]]) -> int:
         n = len(nums)
+        dp = [[0] * 2 for _ in range(n + 1)]
+        dp[n][0] = float("-inf")
 
-        @cache
-        def dp(i, isEven):
-            if i == n:
-                return 0 if isEven else float("-inf")
+        for i in range(n - 1, -1, -1):
+            for j in [0, 1]:
+                dp[i][j] = max(dp[i + 1][j] + nums[i], dp[i + 1][j ^ 1] + (nums[i] ^ k))
 
-            return max(dp(i + 1, isEven) + nums[i], dp(i + 1, isEven ^ 1) + (nums[i] ^ k))
+        return dp[0][1]
 
-        ans = dp(0, True)
-        dp.cache_clear()
-        return ans
+
+        # @cache
+        # def dp(i, isEven):
+        #     if i == n:
+        #         return 0 if isEven else float("-inf")
+
+        #     return max(dp(i + 1, isEven) + nums[i], dp(i + 1, isEven ^ 1) + (nums[i] ^ k))
+
+        # ans = dp(0, True)
+        # dp.cache_clear()
+        # return ans
