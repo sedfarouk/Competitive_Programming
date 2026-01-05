8bb72e95a8c6ae5df63efc9a48895e72c32b8a9f
Farouk Sedick <sadikalhanssah@gmail.com>
2026-01-05 20:09:35 +0000

    Time: 11 ms (31.44%) | Memory: 20.8 MB (78.15%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -3,16 +3,14 @@ class Solution:
         n = len(intervals)
 
         intervals.sort()
+        res = []
         i = 0
-        ans = []
 
         while i < n:
             st, end = intervals[i]
-
-            while i < n and intervals[i][0] <= end:
-                end = max(intervals[i][1], end)
+            while i < n and end >= intervals[i][0]:
+                end = max(end, intervals[i][1])
                 i += 1
+            res.append([st, end])
 
-            ans.append([st, end])
-
-        return ans
+        return res
