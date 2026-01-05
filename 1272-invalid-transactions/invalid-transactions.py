b637115112b3aec0658b3569feab455d07a48c55
Farouk Sedick <sadikalhanssah@gmail.com>
2026-01-05 17:45:44 +0000

    Time: 7 ms (96.40%) | Memory: 18.2 MB (72.80%) - LeetSync

--- 1272-invalid-transactions/invalid-transactions.py
+++ 1272-invalid-transactions/invalid-transactions.py
@@ -4,25 +4,21 @@ class Solution:
         invalid = []
 
         for trans in transactions:
-            name, time, amount, city = trans.split(",")
-            time, amount = int(time), int(amount)
+            name, time, amt, city = trans.split(",")
+            time, amt = int(time), int(amt)
 
-            if city not in history[time][name]:
-                history[time][name].add(city)
+            history[time][name].add(city)
 
         for trans in transactions:
-            name, time, amount, city = trans.split(",")
-            time, amount = int(time), int(amount)
-
-            if amount > 1000:
+            name, time, amt, city = trans.split(",")
+            time, amt = int(time), int(amt)
+            
+            if amt > 1000:
                 invalid.append(trans)
                 continue
 
             for t in range(time - 60, time + 61):
-                if t not in history:
-                    continue
-
-                if name not in history[t]:
+                if t not in history or name not in history[t]:
                     continue
 
                 if len(history[t][name]) > 1 or next(iter(history[t][name])) != city:
@@ -30,4 +26,4 @@ class Solution:
                     break
 
         return invalid
-
+    
