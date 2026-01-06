85cfac28ae61089e8b576716d9b9a81619815e7b
Farouk Sedick <sadikalhanssah@gmail.com>
2026-01-06 15:09:42 +0000

    Time: 0 ms (100.00%) | Memory: 19.5 MB (15.90%) - LeetSync

--- 48-rotate-image/rotate-image.py
+++ 48-rotate-image/rotate-image.py
@@ -3,24 +3,19 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        mat_cpy = deepcopy(matrix)
+        self.transpose(matrix)
+        self.reflect(matrix)
 
-        def copyTo(r1, r2):
-            for i in range(len(r2)):
-                x1, y1 = r1[i]
-                x2, y2 = r2[i]
-                matrix[x2][y2] = mat_cpy[x1][y1]
-
+    def transpose(self, matrix):
         n = len(matrix)
-        for i in range(n // 2):
-            topRow = [(i, j) for j in range(i, n-i)]
-            rightCol = [(j, n-i-1) for j in range(i, n-i)]
-            bottomRow = [(n-i-1, j) for j in range(n-i-1, i-1, -1)]
-            leftCol = [(j, i) for j in range(n-i-1, i-1, -1)]
 
-            copyTo(topRow, rightCol)
-            copyTo(rightCol, bottomRow)
-            copyTo(bottomRow, leftCol)
-            copyTo(leftCol, topRow)
+        for i in range(n):
+            for j in range(i + 1, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
-            
+    def reflect(self, matrix):
+        n = len(matrix)
+
+        for i in range(n):
+            for j in range(n // 2):
+                matrix[i][j], matrix[i][n - j - 1] = matrix[i][n - j - 1], matrix[i][j]
