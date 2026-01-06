625629c5fa2dcadd282f4e07e5c419dd14811233
Farouk Sedick <sadikalhanssah@gmail.com>
2026-01-06 16:17:38 +0000

    Time: 47 ms (8.23%) | Memory: 19.4 MB (11.41%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -1,18 +1,16 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        mult = 1 if x > 0 else -1
-        x *= mult
-        mx = 2147483647 
+        MAXX = 2 ** 31
+        neg = x < 0
+        x = abs(x)
+        res = 0
 
-        num = 0
         while x:
-            rem = x % 10
-            x //= 10
-            
-            if num > (mx - rem) // 10:
+            r = x % 10
+            if res > (MAXX - r) // 10:
                 return 0
-            num = (num * 10) + rem
+            
+            res = res * 10 + r
+            x //= 10
 
-        return num * mult
-
-
+        return res if not neg else -res
