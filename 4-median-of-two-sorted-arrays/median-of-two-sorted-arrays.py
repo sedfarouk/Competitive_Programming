bf37bfe0bb9e0f838c5d3db7c1a357cd6a9058b8
Farouk Sedick <sadikalhanssah@gmail.com>
2026-01-06 22:28:39 +0000

    Time: 4 ms (33.77%) | Memory: 19.6 MB (13.42%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -1,26 +1,29 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        if len(nums1) > len(nums2): # for O(log(min(n, m)))
+        if len(nums1) > len(nums2):
             nums1, nums2 = nums2, nums1
 
         l1, l2 = len(nums1), len(nums2)
-        l, r = 0, l1
+        l, r = 0, len(nums1)
         while l <= r:
-            p1 = l + (r - l) // 2
-            p2 = (l1 + l2 + 1) // 2 - p1
+            ptr1 = l + (r - l) // 2
+            ptr2 = (l1 + l2 + 1) // 2 - ptr1
 
-            max_left1 = float("-inf") if p1 == 0 else nums1[p1 - 1]
-            min_right1 = float("inf") if p1 == l1 else nums1[p1]
-            max_left2 = float("-inf") if p2 == 0 else nums2[p2 - 1]
-            min_right2 = float("inf") if p2 == l2 else nums2[p2]
+            max_left1 = float("-inf") if ptr1 == 0 else nums1[ptr1 - 1]
+            max_left2 = float("-inf") if ptr2 == 0 else nums2[ptr2 - 1]
+            min_right1 = float("inf") if ptr1 == l1 else nums1[ptr1]
+            min_right2 = float("inf") if ptr2 == l2 else nums2[ptr2]
 
-            if max_left1 <= min_right2 and max_left2 <= min_right1:
-                if (l1 + l2) % 2 == 0:
-                    return (max(max_left1, max_left2) + min(min_right1, min_right2)) / 2
-                return max(max_left1, max_left2)
+            if min_right1 >= max_left2 and min_right2 >= max_left1:
+                if (l1 + l2) % 2:
+                    return max(max_left1, max_left2)
+                
+                return (max(max_left1, max_left2) + min(min_right1, min_right2)) / 2
 
             elif max_left1 > min_right2:
-                r = p1 - 1
-
+                r = ptr1 - 1
+            
             else:
-                l = p1 + 1
+                l = ptr1 + 1
+
+        
