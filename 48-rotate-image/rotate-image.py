10551f60f674bf68528c2124c34e62ba7e2c6a92
Farouk Sedick <sadikalhanssah@gmail.com>
2026-01-06 15:26:46 +0000

    Time: 0 ms (100.00%) | Memory: 19.2 MB (15.90%) - LeetSync

--- 48-rotate-image/rotate-image.py
+++ 48-rotate-image/rotate-image.py
@@ -3,8 +3,17 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        self.transpose(matrix)
-        self.reflect(matrix)
+        # self.transpose(matrix)
+        # self.reflect(matrix)
+
+        n = len(matrix)
+        for i in range(n // 2 + n % 2):
+            for j in range(n // 2):
+                tmp = matrix[n - 1 - j][i]
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j]
+                matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i]
+                matrix[j][n - 1 - i] = matrix[i][j]
+                matrix[i][j] = tmp
 
     def transpose(self, matrix):
         n = len(matrix)
