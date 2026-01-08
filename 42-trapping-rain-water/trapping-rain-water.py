8105f4622544c38c39e0e08a700f9e97aaff3904
Farouk Sedick <sadikalhanssah@gmail.com>
2026-01-08 09:08:31 +0000

    Time: 6 ms (88.47%) | Memory: 21.2 MB (5.03%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -2,15 +2,31 @@ class Solution:
     def trap(self, height: List[int]) -> int:
         n = len(height)
         ans = 0
-        prefixMax, suffixMax = [height[0]], [height[-1]]
 
-        for i in range(1, n):
-            prefixMax.append(max(prefixMax[-1], height[i]))
+        left, right = 0, n - 1
+        leftMax = rightMax = 0
 
-        for i in range(n - 2, -1, -1):
-            suffixMax.append(max(suffixMax[-1], height[i]))
-
-        for i in range(n):
-            ans += max(0, min(prefixMax[i], suffixMax[n - i - 1]) - height[i])
-
+        while left < right:
+            if height[left] < height[right]:
+                leftMax = max(leftMax, height[left])
+                ans += leftMax - height[left]
+                left += 1
+            else:
+                rightMax = max(rightMax, height[right])
+                ans += rightMax - height[right]
+                right -= 1
+            
         return ans
+
+        # prefixMax, suffixMax = [height[0]], [height[-1]]
+
+        # for i in range(1, n):
+        #     prefixMax.append(max(prefixMax[-1], height[i]))
+
+        # for i in range(n - 2, -1, -1):
+        #     suffixMax.append(max(suffixMax[-1], height[i]))
+
+        # for i in range(n):
+        #     ans += max(0, min(prefixMax[i], suffixMax[n - i - 1]) - height[i])
+
+        # return ans
