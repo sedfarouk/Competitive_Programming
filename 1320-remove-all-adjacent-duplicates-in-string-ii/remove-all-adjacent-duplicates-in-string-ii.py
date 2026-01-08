86b4631ed84035de672d74d7ea93a645cac3bede
Farouk Sedick <sadikalhanssah@gmail.com>
2026-01-08 15:23:15 +0000

    Time: 34 ms (98.85%) | Memory: 24 MB (8.87%) - LeetSync

--- 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.py
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.py
@@ -5,15 +5,11 @@ class Solution:
         for ch in s:
             if not stack or stack[-1][0] != ch:
                 stack.append([ch, 1])
-
+            
             else:
-                if stack[-1][0] == ch and stack[-1][1] == k - 1:
+                if stack[-1][1] == k - 1:
                     stack.pop()
                 else:
                     stack[-1][1] += 1
 
-        ans = ""
-        for ch, f in stack:
-            ans += ch * f
-
-        return ans
+        return "".join([x[0] * x[1] for x in stack])
