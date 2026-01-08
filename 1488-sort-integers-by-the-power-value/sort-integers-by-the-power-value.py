1a750e91ce6eb41b7c1557356d41c4155a7e368f
Farouk Sedick <sadikalhanssah@gmail.com>
2026-01-08 16:16:52 +0000

    Time: 87 ms (84.94%) | Memory: 45.9 MB (5.04%) - LeetSync

--- 1488-sort-integers-by-the-power-value/sort-integers-by-the-power-value.py
+++ 1488-sort-integers-by-the-power-value/sort-integers-by-the-power-value.py
@@ -1,27 +1,12 @@
 class Solution:
     def getKth(self, lo: int, hi: int, k: int) -> int:
-        memo = {}        
-        def findPower(x):
-            if x in memo:
-                return memo[x]
-
+        @cache
+        def powerValue(x):
             if x == 1:
                 return 0
 
             if x % 2:
-                memo[x] =  findPower(x * 3 + 1) + 1
-            else:
-                memo[x] = findPower(x // 2) + 1
-            return memo[x]
+                return powerValue(x * 3 + 1) + 1
+            return powerValue(x // 2) + 1
 
-        heap = []
-        for i in range(lo, hi + 1):
-            power = findPower(i)
-
-            if len(heap) < k:
-                heappush(heap, (-power, -i))
-
-            elif -heap[0][0] > power:
-                heapreplace(heap, (-power, -i))
-
-        return -heap[0][1]
+        return sorted([*range(lo, hi + 1)], key=lambda x:powerValue(x))[k - 1]
