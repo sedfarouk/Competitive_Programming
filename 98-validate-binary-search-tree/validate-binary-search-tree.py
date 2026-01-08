7d1dda67087debc2539055832badc53ab15fc3a1
Farouk Sedick <sadikalhanssah@gmail.com>
2026-01-08 15:47:44 +0000

    Time: 1 ms (45.80%) | Memory: 21 MB (5.38%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/validate-binary-search-tree.py
@@ -5,14 +5,30 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    def __init__(self):
+        self.prev = float("-inf")
+
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        def validate(node, left = float("-inf"), right = float("inf")):
-            if not node:
+        # in-order
+        def inOrder(root):
+            if not root:
                 return True
-
-            if node.val <= left or node.val >= right:
+            if not inOrder(root.left):
                 return False
+            if root.val <= self.prev:
+                return False
+            self.prev = root.val
+            return inOrder(root.right)
+        
+        return inOrder(root)
+        
+        # def validate(node, left = float("-inf"), right = float("inf")):
+        #     if not node:
+        #         return True
 
-            return validate(node.right, node.val, right) and validate(node.left, left, node.val)
+        #     if node.val <= left or node.val >= right:
+        #         return False
 
-        return validate(root)
+        #     return validate(node.right, node.val, right) and validate(node.left, left, node.val)
+
+        # return validate(root)
