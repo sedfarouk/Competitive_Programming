cbc111974b1d117d06b93b9358f77101a69f002d
Farouk Sedick <sadikalhanssah@gmail.com>
2026-01-08 16:00:27 +0000

    Time: 48 ms (87.11%) | Memory: 19.8 MB (5.02%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.py
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.py
@@ -1,21 +1,28 @@
 class Solution:
     def firstUniqChar(self, s: str) -> int:
-        st = set()
-        seen = set()
+        cnt = Counter(s)
         n = len(s)
 
         for i in range(n):
-            present = s[i] in seen 
-
-            if present and s[i] in st:
-                st.remove(s[i])
-            elif not present:
-                st.add(s[i])
-                seen.add(s[i])
-
-        for i in range(n):
-            if s[i] in st:
+            if cnt[s[i]] == 1:
                 return i
         return -1
+        
+        # st = set()
+        # seen = set()
+
+        # for i in range(n):
+        #     present = s[i] in seen 
+
+        #     if present and s[i] in st:
+        #         st.remove(s[i])
+        #     elif not present:
+        #         st.add(s[i])
+        #         seen.add(s[i])
+
+        # for i in range(n):
+        #     if s[i] in st:
+        #         return i
+        # return -1
 
             
