2a1738428e95ff809a598d69cab02a791f3008e4
Farouk Sedick <sadikalhanssah@gmail.com>
2026-01-08 21:56:22 +0000

    Time: 3 ms (81.08%) | Memory: 19.4 MB (5.95%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,36 +1,40 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         n, m = len(grid), len(grid[0])
-        dirs = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+        dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
         queue = deque()
+        ans = fresh = 0
 
-        def inbound(r, c):
-            return 0 <= r < n and 0 <= c < m
-
-        fresh = 0
         for i in range(n):
             for j in range(m):
                 if grid[i][j] == 2:
                     queue.append((i, j))
+                    grid[i][j] = 0
+                
                 elif grid[i][j] == 1:
                     fresh += 1
 
-        ans = -1
+        def inbounds(r, c):
+            return 0 <= r < n and 0 <= c < m
+
+        if fresh == 0:
+            return 0
+        
         while queue:
             l = len(queue)
+            old_fresh = fresh
 
             for _ in range(l):
                 r, c = queue.popleft()
 
                 for dr, dc in dirs:
-                    nr, nc = dr + r,  c + dc
+                    nr, nc = r + dr, c + dc
 
-                    if inbound(nr, nc) and grid[nr][nc]==1:
-                        grid[nr][nc] = 2
+                    if inbounds(nr, nc) and grid[nr][nc] == 1:
                         fresh -= 1
+                        grid[nr][nc] = 0
                         queue.append((nr, nc))
 
-            ans += 1
-        return max(0, ans) if fresh == 0 else -1
+            ans += old_fresh != fresh
 
-        
+        return ans if fresh == 0 else -1
