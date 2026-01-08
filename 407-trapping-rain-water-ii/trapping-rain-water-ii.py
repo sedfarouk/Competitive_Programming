62b0d56d2cf4f1f876bef84136d88dc579311e20
Farouk Sedick <sadikalhanssah@gmail.com>
2026-01-08 09:33:09 +0000

    Time: 124 ms (65.30%) | Memory: 21.8 MB (26.19%) - LeetSync

--- 407-trapping-rain-water-ii/trapping-rain-water-ii.py
+++ 407-trapping-rain-water-ii/trapping-rain-water-ii.py
@@ -1,12 +1,17 @@
 class Solution:
     def trapRainWater(self, heightMap: List[List[int]]) -> int:
         n, m = len(heightMap), len(heightMap[0])
+
+        if n < 3 or m < 3:
+            return 0
+
+        def inbounds(r, c):
+            return 0 <= r < n and 0 <= c < m
+
+        dirs = [(0, 1), (1, 0), (-1, 0), (0, -1)]
         vis = [[False] * m for _ in range(n)]
-
-        if n < 3 or m < 3: return 0
-
-        dirs = [(1, 0), (0, 1), (-1, 0), (0, -1)]
         queue = []
+        ans = 0
 
         for i in range(1, m - 1):
             heappush(queue, (heightMap[0][i], 0, i))
@@ -18,10 +23,6 @@ class Solution:
             heappush(queue, (heightMap[i][-1], i, m - 1))
             vis[i][0] = vis[i][-1] = True
 
-        def inbounds(r, c):
-            return 0 <= r < n and 0 <= c < m
-
-        ans = 0
         while queue:
             curr, r, c = heappop(queue)
 
@@ -32,9 +33,8 @@ class Solution:
                     ans += max(0, curr - heightMap[nr][nc])
                     heappush(queue, (max(curr, heightMap[nr][nc]), nr, nc))
                     vis[nr][nc] = True
+        
+        return ans
 
-        return ans 
-
-
-
+        
 
