c400d97825b8f4f5f795c4c0689d559673ea4a61
Farouk Sedick <sadikalhanssah@gmail.com>
2026-01-08 15:15:00 +0000

    Time: 101 ms (91.06%) | Memory: 79.6 MB (6.49%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,24 +1,56 @@
+class DoublyLinkedListNode:
+    def __init__(self, key = -1, val = -1, next = None, prev = None):
+        self.key = key
+        self.val = val
+        self.next = next
+        self.prev = prev
+
 class LRUCache:
 
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.cache = OrderedDict()
+        self.history = {}
+        self.head = self.tail = DoublyLinkedListNode()
 
     def get(self, key: int) -> int:
-        if key not in self.cache:
+        if key not in self.history:
             return -1
 
-        self.cache.move_to_end(key)
-        return self.cache[key]
+        self.updateHead(self.history[key])
+        return self.head.val
+
 
     def put(self, key: int, value: int) -> None:
-        if key in self.cache:
-            self.cache.move_to_end(key)
+        if key not in self.history:
+            self.history[key] = self.insert(key, value)
+            
+            if len(self.history) > self.capacity:
+                del self.history[self.tail.next.key]
+                self.tail = self.tail.next
 
-        self.cache[key] = value
-        if len(self.cache) > self.capacity:
-            self.cache.popitem(False)
+        self.updateHead(self.history[key])
+        self.head.val = value
 
+
+    def insert(self, key, val):
+        newNode = DoublyLinkedListNode(key, val)
+        newNode.prev = self.head
+        self.head.next = newNode
+        self.head = newNode
+
+        return newNode
+
+    
+    def updateHead(self, node):
+        if node == self.head:
+            return
+
+        node.prev.next = node.next
+        node.next.prev = node.prev
+        self.head.next = node
+        node.prev = self.head
+        node.next = None
+        self.head = node
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
