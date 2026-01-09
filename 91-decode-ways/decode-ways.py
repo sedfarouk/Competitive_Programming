e70fa0181491e1743ba594c9565777fdf92534cb
Farouk Sedick <sadikalhanssah@gmail.com>
2026-01-09 21:14:04 +0000

    Time: 3 ms (29.22%) | Memory: 19.4 MB (5.14%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -2,18 +2,37 @@ class Solution:
     def numDecodings(self, s: str) -> int:
         n = len(s)
 
-        @lru_cache
-        def dp(i):
-            if i == n:
-                return 1
+        if s[0] == '0':
+            return 0
 
-            ans = 0
+        one_back = two_back = 1
+
+        for i in range(1, n):
+            current = 0
+
             if s[i] != '0':
-                ans = dp(i + 1)
-                if i < n - 1 and int(s[i:i+2]) < 27:
-                    ans += dp(i + 2)
+                current = one_back
+            
+            two_digit = int(s[i-1:i+1])
+            if 26 >= two_digit >= 10:
+                current += two_back
+            two_back = one_back
+            one_back = current
 
-            return ans
+        return one_back
 
-        return dp(0)
+        # @lru_cache
+        # def dp(i):
+        #     if i == n:
+        #         return 1
+
+        #     ans = 0
+        #     if s[i] != '0':
+        #         ans = dp(i + 1)
+        #         if i < n - 1 and int(s[i:i+2]) < 27:
+        #             ans += dp(i + 2)
+
+        #     return ans
+
+        # return dp(0)
                 
