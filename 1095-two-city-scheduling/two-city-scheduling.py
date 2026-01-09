639c1f6bb906110cdcae592f090994ce6be5cef4
Farouk Sedick <sadikalhanssah@gmail.com>
2026-01-09 00:36:40 +0000

    Time: 0 ms (100.00%) | Memory: 19.3 MB (5.04%) - LeetSync

--- 1095-two-city-scheduling/two-city-scheduling.py
+++ 1095-two-city-scheduling/two-city-scheduling.py
@@ -1,19 +1,15 @@
 class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
         n = len(costs) // 2
+        costs.sort(key=lambda x: abs(x[0] - x[1]), reverse=True)
+        ans = a = b = 0
 
-        @cache
-        def dp(i, c):
-            if c > n:
-                return float("inf")
+        for x, y in costs:
+            if (a < n and x < y) or b == n:
+                ans += x
+                a += 1
+            else:
+                ans += y
+                b += 1
 
-            if i == 2 * n:
-                return 0 if c == n else float("inf")
-
-            return min(dp(i+1, c+1) + costs[i][0], dp(i+1, c) + costs[i][1])
-
-        res = dp(0, 0)
-        dp.cache_clear()
-        return res
-            
-        
+        return ans
