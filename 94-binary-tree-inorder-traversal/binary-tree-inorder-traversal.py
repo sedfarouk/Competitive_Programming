02c2cdba21b3269bddc076a0ce3475e74eed01f6
Farouk Sedick <sadikalhanssah@gmail.com>
2026-01-09 18:15:05 +0000

    Time: 0 ms (100.00%) | Memory: 19.5 MB (10.43%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.py
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.py
@@ -9,8 +9,31 @@ class Solution:
         if not root:
             return []
 
-        lst = self.inorderTraversal(root.left)
-        lst.append(root.val)
-        lst += self.inorderTraversal(root.right)
+        stack = [(root, 1)]
+        res = []
+        while stack:
+            node, direction = stack.pop()
 
-        return lst
+            if direction == -1:
+                res.append(node.val)
+                continue
+
+            if node.right:
+                stack.append((node.right, 1))
+
+            stack.append((node, -1))
+
+            if node.left:
+                stack.append((node.left, 1))
+
+        return res
+        
+        # if not root:
+        #     return []
+
+        # lst = self.inorderTraversal(root.left)
+        # lst.append(root.val)
+        # lst += self.inorderTraversal(root.right)
+
+        # return lst
+
