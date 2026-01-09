0284f2ab9ba26e60f4cf1c11491a4e4f7b2d099b
Farouk Sedick <sadikalhanssah@gmail.com>
2026-01-09 02:38:17 +0000

    Time: 3 ms (47.52%) | Memory: 19.4 MB (11.59%) - LeetSync

--- 67-add-binary/add-binary.py
+++ 67-add-binary/add-binary.py
@@ -1,3 +1,26 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        return bin(int(a, 2) + int(b, 2))[2:]
+        n = max(len(a), len(b))
+        a, b = a.zfill(n), b.zfill(n)
+        ans = []
+        carry = 0
+
+        for i in range(n - 1, -1, -1):
+            if a[i] == '1':
+                carry += 1
+            if b[i] == '1':
+                carry += 1
+
+            if carry % 2:
+                ans.append('1')
+            else:
+                ans.append('0')
+            
+            carry //= 2
+
+        if carry:
+            ans.append('1')
+
+        return "".join(ans[::-1])
+
+        
