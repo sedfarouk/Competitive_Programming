9c4102a284cb13d1e03626b247c8107ad32d7d8b
Farouk Sedick <sadikalhanssah@gmail.com>
2026-01-09 04:45:21 +0000

    Time: 3 ms (52.53%) | Memory: 19.6 MB (15.23%) - LeetSync

--- 692-top-k-frequent-words/top-k-frequent-words.py
+++ 692-top-k-frequent-words/top-k-frequent-words.py
@@ -1,4 +1,24 @@
+class Element:
+    def __init__(self, word, freq):
+        self.word = word
+        self.freq = freq
+
+    def __lt__(self, other):
+        if self.freq == other.freq:
+            return self.word > other.word 
+        return self.freq < other.freq
+
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
         cnt = Counter(words)
-        return sorted(list(cnt.keys()), key=lambda x: (-cnt[x], x))[:k]
+        heap = []
+
+        for word, freq in cnt.items():
+            if len(heap) < k:
+                heappush(heap, Element(word, freq))
+
+            elif freq > heap[0].freq or (freq == heap[0].freq and word < heap[0].word):
+                heapreplace(heap, Element(word, freq))
+
+        heap.sort(reverse=True)
+        return [element.word for element in heap]
