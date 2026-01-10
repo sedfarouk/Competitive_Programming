f60a1893ac8607170c990ea22976a40dbdc9e89c
Farouk Sedick <sadikalhanssah@gmail.com>
2026-01-10 23:25:52 +0000

    Time: 119 ms (50.39%) | Memory: 32.3 MB (17.98%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.py
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.py
@@ -1,11 +1,7 @@
 class NumMatrix:
 
     def __init__(self, matrix: List[List[int]]):
-        self.mat = [[0] * (len(matrix[0]) + 1)] + matrix
-
-        for i in range(1, len(matrix) + 1):
-            self.mat[i] = [0] + self.mat[i]
-
+        self.mat = matrix
         n, m = len(self.mat), len(self.mat[0])
 
         for r in self.mat:
@@ -17,11 +13,11 @@ class NumMatrix:
                 self.mat[r][c] += self.mat[r - 1][c]
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        left = self.mat[row2 + 1][col1] 
-        top = self.mat[row1][col2 + 1]
-        corner = self.mat[row1][col1]
+        left = self.mat[row2][col1 - 1] if (row2 >= 0 and col1 > 0) else 0
+        top = self.mat[row1 - 1][col2] if (row1 > 0 and col2 >= 0) else 0
+        corner = self.mat[row1 - 1][col1 - 1] if (row1 > 0 and col1 > 0) else 0
 
-        return self.mat[row2 + 1][col2 + 1] - (left + top - corner)
+        return self.mat[row2][col2] - (left + top - corner)
 
 
 # Your NumMatrix object will be instantiated and called as such:
