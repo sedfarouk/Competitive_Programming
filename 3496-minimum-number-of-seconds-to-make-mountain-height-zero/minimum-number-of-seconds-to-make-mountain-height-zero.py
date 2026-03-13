00b17c24993f4aac95d45b629f7e4e5b9199ee8e
Farouk Sedick <sadikalhanssah@gmail.com>
2026-03-13 23:19:25 +0000

    Time: 246 ms (88.34%) | Memory: 20.6 MB (90.18%) - LeetSync

--- 3496-minimum-number-of-seconds-to-make-mountain-height-zero/minimum-number-of-seconds-to-make-mountain-height-zero.py
+++ 3496-minimum-number-of-seconds-to-make-mountain-height-zero/minimum-number-of-seconds-to-make-mountain-height-zero.py
@@ -1,33 +1,18 @@
 class Solution:
     def minNumberOfSeconds(self, mH: int, wT: List[int]) -> int:
-        n = len(wT)
-        wT.sort()
-
-        def bs(lim, w):
-            l = rem = 0
-            r = lim
-            while l <= r:
-                m = l + (r - l) // 2
-
-                if w * (m * (m + 1) // 2) <= lim:
-                    rem = m
-                    l = m + 1
-                else:
-                    r = m - 1
-            return rem 
-
-        def f(ux):
+        def f(T):
             h = mH
 
-            for i in range(n):
-                rem = bs(ux, wT[i])
-                h -= rem
+            for w in wT:
+                k = int((math.sqrt(1 + 8*T//w) - 1) // 2)
+                h -= k
 
                 if h <= 0:
                     return True
+
             return False
 
-        l, r = 1, (mH * (mH + 1) // 2) * max(wT)
+        l, r = 1, max(wT) * (mH * (mH + 1) // 2)
         ans = r
 
         while l <= r:
