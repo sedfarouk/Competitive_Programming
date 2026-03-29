a9688bf79dc72111aa4b9360833cbf597e09b19e
Farouk Sedick <sadikalhanssah@gmail.com>
2026-03-29 16:15:28 +0000

    Time: 2159 ms (48.63%) | Memory: 52.1 MB (5.03%) - LeetSync

--- 4257-sum-of-sortable-integers/sum-of-sortable-integers.py
+++ 4257-sum-of-sortable-integers/sum-of-sortable-integers.py
@@ -1,36 +1,51 @@
 MAXX = 10 ** 5
+
 DIVS = [[] for _ in range(MAXX + 1)]
 for i in range(1, MAXX + 1):
     for j in range(i, MAXX + 1, i):
         DIVS[j].append(i)
 
+
 class Solution:
     def sortableIntegers(self, nums: list[int]) -> int:
         n = len(nums)
         ans = 0
+        arr = nums
 
         for k in DIVS[n]:
             sortable = True
-            prevMx = prevMn = 0
-            for i in range(0, n - k + 1, k):
+            prevMx = -10**18
+            prevMn = -10**18
+
+            for i in range(0, n, k):
                 br = 0
-                currMx = currMn = nums[i]
-                for j in range(i + 1, i + k):
-                    if nums[j] < nums[j - 1]:
+                currMx = currMn = arr[i]
+
+                for j in range(1, k):
+                    cur = arr[i + j]
+                    prev = arr[i + j - 1]
+
+                    if cur < prev:
                         br += 1
-                        if br > 1 or nums[i] < max(nums[j:i+k]): 
-                            br += 1
+                        if br > 1:
                             break
 
-                    currMx = max(currMx, nums[j])
-                    currMn = min(currMn, nums[j])
-                
+                    if cur > currMx:
+                        currMx = cur
+                    if cur < currMn:
+                        currMn = cur
+
+                if br == 1 and arr[i] < arr[i + k - 1]:
+                    br += 1
+
                 if br > 1 or prevMx > currMx or prevMn > currMn or prevMx > currMn:
                     sortable = False
                     break
-                prevMx = currMx; prevMn = currMn
-            ans += k if sortable else 0
-        
-        return ans 
-            
-            
+
+                prevMx = currMx
+                prevMn = currMn
+
+            if sortable:
+                ans += k
+
+        return ans
