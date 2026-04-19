c400b41cffe6b3158468e7ec4409048915e4d1e9
Farouk Sedick <sadikalhanssah@gmail.com>
2026-04-19 09:38:05 +0000

    Time: 829 ms (64.76%) | Memory: 34.3 MB (91.57%) - LeetSync

--- 4283-multi-source-flood-fill/multi-source-flood-fill.py
+++ 4283-multi-source-flood-fill/multi-source-flood-fill.py
@@ -1,7 +1,6 @@
 class Solution:
     def colorGrid(self, n: int, m: int, sources: list[list[int]]) -> list[list[int]]:
-        q = [(0, -sources[i][2], sources[i][0], sources[i][1]) for i in range(len(sources))]
-        heapify(q)
+        q = deque(sorted([(sources[i][2], sources[i][0], sources[i][1]) for i in range(len(sources))], reverse=True))
         grid = [[0] * m for _ in range(n)]
 
         for r, c, v in sources:
@@ -11,18 +10,16 @@ class Solution:
             return 0 <= r < n and 0 <= c < m
 
         dirs = [(0, 1), (1, 0), (-1, 0), (0, -1)]
-        curr = 1
         while q:
             for _ in range(len(q)):
-                _, v, i, j = heappop(q)
+                v, i, j = q.popleft()
 
                 for dr, dc in dirs:
                     nr, nc = i + dr, j + dc
 
                     if inbound(nr, nc) and grid[nr][nc] == 0:
-                        grid[nr][nc] = -v
-                        heappush(q, (curr, v, nr, nc))
-            curr += 1
+                        grid[nr][nc] = v
+                        q.append((v, nr, nc))
 
         return grid
 
