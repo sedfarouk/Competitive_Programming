8be8179ae8818c7935b52fcdb09dfff346caa8e7
Farouk Sedick <sadikalhanssah@gmail.com>
2026-04-19 07:07:39 +0000

    Time: 861 ms (5.03%) | Memory: 58.1 MB (5.03%) - LeetSync

--- 4139-minimum-absolute-distance-between-mirror-pairs/minimum-absolute-distance-between-mirror-pairs.py
+++ 4139-minimum-absolute-distance-between-mirror-pairs/minimum-absolute-distance-between-mirror-pairs.py
@@ -3,10 +3,20 @@ class Solution:
         h = defaultdict(lambda: float("-inf"))
         ans = float("inf")
 
+        def reverseNum(num):
+            rev = 0
+
+            while num > 0:
+                rev = rev * 10 + (num % 10)
+                num //= 10
+            
+            return rev
+
         for idx, num in enumerate(nums):
-            x = str(num)[::-1]
-            ans = min(ans, idx - h[x])
-            h[str(int(str(num)[::-1]))[::-1]] = idx
+            if num % 10 != 0:
+                x = reverseNum(num)
+                ans = min(ans, idx - h[x])
+            h[reverseNum(reverseNum(num))] = idx
 
         return ans if ans != float("inf") else -1
             
