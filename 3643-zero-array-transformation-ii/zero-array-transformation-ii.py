761ceae836e31fc6dac285f6b2bbdded8d30dac7
Farouk Sedick <sadikalhanssah@gmail.com>
2026-05-13 19:39:34 +0000

    Time: 1510 ms (5.63%) | Memory: 64.6 MB (99.13%) - LeetSync

--- 3643-zero-array-transformation-ii/zero-array-transformation-ii.py
+++ 3643-zero-array-transformation-ii/zero-array-transformation-ii.py
@@ -3,7 +3,6 @@ class Solution:
         n, m = len(nums), len(queries)
 
         def f(x):
-            num_cpy = nums[:]
             diff = [0] * (n + 2)
 
             for i in range(x):
@@ -13,9 +12,8 @@ class Solution:
 
             for i in range(1, n + 1):
                 diff[i] += diff[i - 1]
-                num_cpy[i - 1] += diff[i]
 
-                if num_cpy[i - 1] > 0: return False
+                if (nums[i - 1] + diff[i]) > 0: return False
             
             return True
 
