df647b2d66c23043d2382a3966e1e4502b779725
Farouk Sedick <sadikalhanssah@gmail.com>
2026-05-13 19:38:08 +0000

    Time: 1616 ms (5.63%) | Memory: 66.2 MB (53.68%) - LeetSync

---
+++ 3643-zero-array-transformation-ii/zero-array-transformation-ii.py
@@ -0,0 +1,33 @@
+class Solution:
+    def minZeroArray(self, nums: List[int], queries: List[List[int]]) -> int:
+        n, m = len(nums), len(queries)
+
+        def f(x):
+            num_cpy = nums[:]
+            diff = [0] * (n + 2)
+
+            for i in range(x):
+                l, r, d = queries[i]
+                
+                diff[l + 1] -= d; diff[r + 2] += d
+
+            for i in range(1, n + 1):
+                diff[i] += diff[i - 1]
+                num_cpy[i - 1] += diff[i]
+
+                if num_cpy[i - 1] > 0: return False
+            
+            return True
+
+        l, r = 0, m
+        ans = -1
+        while l <= r:
+            m = l + (r - l) // 2
+
+            if f(m):
+                r = m - 1
+                ans = m
+            else:
+                l = m + 1
+        
+        return ans
