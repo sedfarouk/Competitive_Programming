356c11b2c07b2ddceadf1090a6561ed04ff45b90
Farouk Sedick <sadikalhanssah@gmail.com>
2026-05-22 23:38:00 +0000

    Time: 0 ms (100.00%) | Memory: 19.4 MB (77.83%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,8 +1,6 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         n = len(nums)
-        l, r = 0, n - 1
-        mid = -1
 
         def binarySearch(l, r):
             while l <= r:
@@ -14,20 +12,19 @@ class Solution:
             
             return -1
 
-        while l <= r:
-            m = l + (r - l) // 2
+        def findPivot(nums):
+            l, r = 0, len(nums) - 1
+            while l < r:
+                m = (l + r) // 2
+                if nums[m] > nums[r]:
+                    l = m + 1
+                else:
+                    r = m
+            return l
 
-            if m < n - 1 and nums[m] > nums[m + 1]:
-                mid = m
-                break
-            
-            elif nums[m] > nums[-1]:
-                l = m + 1
+        pivot = findPivot(nums)
 
-            else:
-                r = m - 1
-
-        return max(binarySearch(0, mid), binarySearch(mid + 1, n - 1))
+        return max(binarySearch(0, pivot - 1), binarySearch(pivot, n - 1))  
 
 
         
