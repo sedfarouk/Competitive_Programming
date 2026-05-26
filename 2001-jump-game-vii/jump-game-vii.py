a8bd01988718972a6a3d8f6fd4a06c40c7bc5c72
Farouk Sedick <sadikalhanssah@gmail.com>
2026-05-26 00:17:34 +0000

    Time: 143 ms (73.60%) | Memory: 20.6 MB (79.70%) - LeetSync

--- 2001-jump-game-vii/jump-game-vii.py
+++ 2001-jump-game-vii/jump-game-vii.py
@@ -5,9 +5,12 @@ class Solution:
 
         dp = [False] * n
         dp[0] = True
-        count = 0
+        maxReach, count = maxJump, 0
 
         for i in range(minJump, n):
+            if i > maxReach:
+                return False
+
             count += dp[i - minJump]
 
             if i > maxJump:
@@ -15,5 +18,6 @@ class Solution:
             
             if count > 0 and s[i] == '0':
                 dp[i] = True
+                maxReach = i + maxJump
         
         return count > 0
