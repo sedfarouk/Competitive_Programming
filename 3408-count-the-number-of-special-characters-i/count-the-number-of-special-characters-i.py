ba839f01a911a7dce67af3a281c73e1da1df3dc5
Farouk Sedick <sadikalhanssah@gmail.com>
2026-05-26 11:59:07 +0000

    Time: 4 ms (9.02%) | Memory: 19.3 MB (64.16%) - LeetSync

--- 3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.py
+++ 3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.py
@@ -1,13 +1,11 @@
 class Solution:
     def numberOfSpecialChars(self, word: str) -> int:
-        s0, s1, s2 = set(), set(), set()
+        s1, s2 = set(), set()
 
         for c in word:
             if c.islower():
                 s1.add(c)
             else:
-                s2.add(c)
-            if c.lower() in s1 and c.upper() in s2:
-                s0.add(c.lower())
+                s2.add(c.lower())
 
-        return len(s0)
+        return len(s1 & s2)
