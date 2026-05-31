075eb37ceac0bbbaa93078b5775c6596730f4609
Farouk Sedick <sadikalhanssah@gmail.com>
2026-05-31 02:28:42 +0000

    Time: 83 ms (31.48%) | Memory: 33.8 MB (75.43%) - LeetSync

---
+++ 2245-destroying-asteroids/destroying-asteroids.py
@@ -0,0 +1,8 @@
+class Solution:
+    def asteroidsDestroyed(self, mass: int, asteroids: List[int]) -> bool:
+        for a in sorted(asteroids):
+            if mass < a:
+                return False
+            mass += a
+
+        return True
