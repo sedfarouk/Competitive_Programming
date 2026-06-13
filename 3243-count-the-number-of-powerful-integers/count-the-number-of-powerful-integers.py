a64ba8c37322e113ab88f6494bc3d37602cf27f8
Farouk Sedick <sadikalhanssah@gmail.com>
2026-06-13 17:00:59 +0000

    Time: 32 ms (36.54%) | Memory: 22.2 MB (30.77%) - LeetSync

--- 3243-count-the-number-of-powerful-integers/count-the-number-of-powerful-integers.py
+++ 3243-count-the-number-of-powerful-integers/count-the-number-of-powerful-integers.py
@@ -14,9 +14,13 @@ class Solution:
                 lim = min(limit, limit if not tight else int(x[i]))
                 ans = 0
 
-                for d in range(lim + 1):
-                    if n - i <= m and int(s[m - (n - i)]) != d: continue
-                    ans += dp(i + 1, tight and d == int(x[i]))
+                if n - i <= m:
+                    curr = int(s[m - (n - i)])
+                    if lim < curr: return 0
+                    else: ans += dp(i + 1, tight and curr == lim)
+                else:
+                    for d in range(lim + 1):
+                        ans += dp(i + 1, tight and d == int(x[i]))
                 
                 return ans
 
