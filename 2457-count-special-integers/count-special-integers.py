c0673435eda78e45e38f54971b89574575ec0540
Farouk Sedick <sadikalhanssah@gmail.com>
2026-06-13 05:38:41 +0000

    Time: 160 ms (82.96%) | Memory: 20.6 MB (80.74%) - LeetSync

--- 2457-count-special-integers/count-special-integers.py
+++ 2457-count-special-integers/count-special-integers.py
@@ -1,24 +1,20 @@
 class Solution:
     def countSpecialNumbers(self, n: int) -> int:
-        tot = n
         n = list(map(int, str(n)))
 
         @cache
-        def dp(i, tight, mask, valid, start):
-            if i == len(n):
-                return int(valid)
+        def dp(i, tight, mask):
+            if i == len(n): return 1
 
             lim = 9 if not tight else n[i]
             ans = 0
 
             for d in range(lim + 1):
-                new_valid = valid | (mask & (1 << d)) > 0
-                new_start = start | d > 0
-                new_mask = (mask | (1 << d)) if new_start else mask
-        
-                ans += dp(i + 1, tight and d == lim, new_mask, new_valid, new_start)
+                if mask & (1 << d): continue
+                ans += dp(i + 1, tight and d == lim, (mask | (1 << d)) if (mask or d > 0) else mask)
+
             return ans
 
-        res = dp(0, True, 0, False, False)
+        res = dp(0, True, 0)
         dp.cache_clear()
-        return tot - res
+        return res - 1
