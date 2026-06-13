f57307cc6868df9af3949fa79863583f5519f413
Farouk Sedick <sadikalhanssah@gmail.com>
2026-06-13 05:44:52 +0000

    Time: 221 ms (16.30%) | Memory: 20.4 MB (80.74%) - LeetSync

--- 2457-count-special-integers/count-special-integers.py
+++ 2457-count-special-integers/count-special-integers.py
@@ -7,13 +7,7 @@ class Solution:
             if i == len(n): return 1
 
             lim = 9 if not tight else n[i]
-            ans = 0
-
-            for d in range(lim + 1):
-                if mask & (1 << d): continue
-                ans += dp(i + 1, tight and d == lim, (mask | (1 << d)) if (mask or d > 0) else mask)
-
-            return ans
+            return sum(dp(i+1, tight and d==lim, (mask | (1<<d)) if (mask or d>0) else mask) for d in range(lim+1) if not mask & (1<<d))
 
         res = dp(0, True, 0)
         dp.cache_clear()
