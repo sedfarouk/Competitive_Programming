79de46cb175b7ab87161e01b7ae6c8ae4fc2d4a3
Farouk Sedick <sadikalhanssah@gmail.com>
2026-06-13 04:16:11 +0000

    Time: 441 ms (14.69%) | Memory: 21.1 MB (59.32%) - LeetSync

---
+++ 1057-numbers-with-repeated-digits/numbers-with-repeated-digits.py
@@ -0,0 +1,23 @@
+class Solution:
+    def numDupDigitsAtMostN(self, n: int) -> int:
+        n = list(map(int, str(n)))
+
+        @cache
+        def dp(i, tight, mask, valid, start):
+            if i == len(n):
+                return int(valid)
+
+            lim = 9 if not tight else n[i]
+            ans = 0
+
+            for d in range(lim + 1):
+                new_valid = valid | (mask & (1 << d)) > 0
+                new_start = start | d > 0
+                new_mask = (mask | (1 << d)) if new_start else mask
+        
+                ans += dp(i + 1, tight and d == lim, new_mask, new_valid, new_start)
+            return ans
+
+        res = dp(0, True, 0, False, False)
+        dp.cache_clear()
+        return res
