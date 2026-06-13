6bd05f42f5235d4b8145e239c06ad76093309f87
Farouk Sedick <sadikalhanssah@gmail.com>
2026-06-13 02:50:56 +0000

    Time: 3 ms (11.60%) | Memory: 19.7 MB (10.00%) - LeetSync

--- 938-numbers-at-most-n-given-digit-set/numbers-at-most-n-given-digit-set.py
+++ 938-numbers-at-most-n-given-digit-set/numbers-at-most-n-given-digit-set.py
@@ -1,24 +1,19 @@
 class Solution:
     def atMostNGivenDigitSet(self, digits: List[str], n: int) -> int:
-        digits = [int(x) for x in digits]
-
-        n = str(n)
-        l1, l2 = len(n), len(digits)
+        digits = list(map(int, digits))
+        n = list(map(int, str(n)))
 
         @cache
         def dp(i, tight, g):
-            if i == l1:
-                return 0
+            if i == len(n): return 0
 
             lim = 9 if not tight else int(n[i])
-
             ans = 0
-            for j in range(l2):
-                new_g = g or digits[j] > lim
-
-                if new_g and i == l1 - 1:
-                    continue
-                ans += dp(i + 1, tight and digits[j] == lim, new_g) + 1
+            
+            for d in digits:
+                new_g = g or d > lim
+                if new_g and i == len(n) - 1: continue
+                ans += dp(i + 1, tight and d == lim, new_g) + 1
 
             return ans
 
