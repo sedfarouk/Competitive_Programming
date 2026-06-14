a5404a73372fbdf5a4d294381a85a6d17f4dafba
Farouk Sedick <sadikalhanssah@gmail.com>
2026-06-14 12:51:04 +0000

    Time: 40 ms (12.50%) | Memory: 19.3 MB (47.92%) - LeetSync

--- 4054-count-distinct-integers-after-removing-zeros/count-distinct-integers-after-removing-zeros.py
+++ 4054-count-distinct-integers-after-removing-zeros/count-distinct-integers-after-removing-zeros.py
@@ -3,21 +3,20 @@ class Solution:
         digits = list(map(int, str(n)))
         n = len(digits)
 
-        @cache
-        def dp(i, tight, start):
-            if i == n: return 1
+        dp = [[[0] * 2 for j in range(2)] for i in range(n + 1)]
+        dp[n][0][1] = dp[n][1][1] = 1
 
-            lim = 9 if not tight else digits[i]
-            ans = 0
+        for i in range(n - 1, -1, -1):
+            for tight in range(2):
+                lim = 9 if not tight else digits[i]
 
-            for d in range(lim + 1):
-                if start and d == 0: continue
-                if not start: ans += dp(i + 1, tight and d == lim, d > 0)
-                else: ans += dp(i + 1, tight and d == lim, start)
-            
-            return ans
+                for start in range(2):
+                    for d in range(lim + 1):
+                        if start and d == 0:
+                            continue
+                        if not start:
+                            dp[i][tight][start] += dp[i + 1][tight and d == lim][d > 0]
+                        else:
+                            dp[i][tight][start] += dp[i + 1][tight and d == lim][start]
 
-        res = dp(0, True, False) - 1
-        dp.cache_clear()
-        return res
-
+        return dp[0][1][0]
