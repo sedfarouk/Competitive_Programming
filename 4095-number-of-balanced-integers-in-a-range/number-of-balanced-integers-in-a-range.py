d65c65d895e5644540faffe1e05112cb418a4877
Farouk Sedick <sadikalhanssah@gmail.com>
2026-06-14 13:48:40 +0000

    Time: 1246 ms (55.71%) | Memory: 95.4 MB (44.29%) - LeetSync

--- 4095-number-of-balanced-integers-in-a-range/number-of-balanced-integers-in-a-range.py
+++ 4095-number-of-balanced-integers-in-a-range/number-of-balanced-integers-in-a-range.py
@@ -5,15 +5,10 @@ class Solution:
 
             @cache
             def dp(i, tight, curr):
-                if i == n:
-                    return int(curr == 0)
-
+                if i == n: return int(curr == 0)
+                
                 lim = 9 if not tight else x[i]
-                ans = 0
-
-                for d in range(lim + 1):
-                    ans += dp(i + 1, tight and d == lim, curr + (d if i % 2 else -d))
-                return ans
+                return sum(dp(i + 1, tight and d == lim, curr + (d if i % 2 else -d)) for d in range(lim + 1))
 
             return dp(0, True, 0)
 
