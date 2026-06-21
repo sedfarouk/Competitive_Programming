6abf60d49649d1686fbb8651ad088a79a4b917ef
Farouk Sedick <sadikalhanssah@gmail.com>
2026-06-21 14:56:28 +0000

    Time: 311 ms (5.00%) | Memory: 55.5 MB (5.16%) - LeetSync

--- 4004-maximize-sum-of-device-ratings/maximize-sum-of-device-ratings.py
+++ 4004-maximize-sum-of-device-ratings/maximize-sum-of-device-ratings.py
@@ -1,17 +1,19 @@
 class Solution:
     def maxRatings(self, units: List[List[int]]) -> int:
-        us = sorted([sorted(x) for x in units], key=lambda x:(x[1] if len(x) > 1 else x[0]), reverse=True)
+        us = sorted([sorted(x, reverse=True) for x in units], key=lambda x:(x[-2] if len(x) > 1 else x[-1]), reverse=True)
+        print(us)
         ans = 0
 
-        for u in us[:-1]:
+        for u in us:
             if len(u) > 1:
-                ans += u[1]
-                us[-1].append(u[0])
+                ans += u[-2]
+                if u[-1] < us[-1][-1]: us[-1].append(u[-1])
             else:
-                ans += u[0]
-        
-        ans += min(us[-1])
-        return ans
+                ans += u[-1]
+
+        if len(us[-1]) > 1:
+            ans = ans - us[-1][-2] + us[-1][-1]
+        return ans 
 
 
 
